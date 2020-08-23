bf2e7fa295c5d41c04c45efe5c718972c4be6f47
J Francisco Rader <francisco.rader@gmail.com>
2020-08-23 20:35:00 -0300

    Fix prod build

--- phone/src/index.js
+++ phone/src/index.js
@@ -1,23 +1,24 @@
-import '@babel/polyfill';
+import "@babel/polyfill";
 
 import React from "react";
 import ReactDOM from "react-dom";
 import "./main.css";
 import Phone from "./Phone";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <RecoilRoot>
       <Phone />
     </RecoilRoot>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.register();
 serviceWorker.unregister();
