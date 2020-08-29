c804945e00dec65e55854cebafafe7451237475d
RockySouthpaw <55056068+RockySouthpaw@users.noreply.github.com>
2020-08-28 23:06:29 -0400

    Added a toggle comand, removed hide/open
    Command phone:open was used to open the phone and phone:hide was used to hide it. (Obviously kek)
    
    Consolodated it into one command. /phone.
    
    Removed the NUI Message from phone:send, but left the `TriggerEvent('phone:sendContacts')`

--- resources/client/cl_main.lua
+++ resources/client/cl_main.lua
@@ -6,38 +6,46 @@ Citizen.CreateThread(function()
     Citizen.Wait(0)
   end
 end)
+------------------------------------------------------------------------ End of ESX
 
 --RegisterCommand('phone:get', function(source, args, rawCommand)
 --    TriggerServerEvent('phone:server:getCredentials')
 --end, false)
 
-RegisterCommand('phone:open', function(source, args, rawCommand)
-    TriggerServerEvent('phone:getCredentials', source)
+local isPhoneOpen = false
+
+RegisterCommand('phone', function(source) -- Toggles Phone
+    TriggerServerEvent('phone:getCredentials', source) 
+    if isPhoneOpen == false then 
+        isPhoneOpen = true 
+        print("phone is now open") --Left for testing purposes. 
+        SendNUIMessage( -- Shows phone
+            {
+            app = 'PHONE',
+            method = 'setVisibility',
+            data = true
+            }
+        )
+        SetNuiFocus(true, true)
+    else
+        isPhoneOpen = false
+        print("phone is now closed") --Left for testing purposes. 
+        SendNUIMessage( -- Hides phone
+            {
+             app = 'PHONE',
+             method = 'setVisibility',
+             data = false
+            }
+        )
+        SetNuiFocus(false, false)
+    end
 end, false)
+
 
 RegisterNetEvent('phone:send')
 AddEventHandler('phone:send', function()
-    SendNUIMessage(
-        {
-         app = 'PHONE',
-         method = 'setVisibility',
-         data = true
-        }
-    )
-    SetNuiFocus(true, true)
     TriggerEvent('phone:sendContacts')
 end)
-
-RegisterCommand('phone:hide', function(source, args, rawCommand)
-    SendNUIMessage(
-        {
-         app = 'PHONE',
-         method = 'setVisibility',
-         data = false
-        }
-    )
-    SetNuiFocus(false, false)
-end, false)
 
 RegisterNUICallback('phone:close', function()
     SendNUIMessage(
