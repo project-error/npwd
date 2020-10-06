c2e1f17f3ba5643394133fca08cc0ad3496aecd0
erik-sn <nieh.erik@gmail.com>
2020-10-05 21:56:03 -0400

    add minimum load time to the UI on toggling a like

--- phone/src/apps/twitter/components/buttons/LikeButton.js
+++ phone/src/apps/twitter/components/buttons/LikeButton.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, CircularProgress } from "@material-ui/core";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 
 import Nui from "../../../../os/nui-events/utils/Nui";
 
+const LOADING_TIME = 1250;
+
 function LikeButton({ tweetId, isLiked }) {
   const [liked, setLiked] = useState(isLiked);
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
     Nui.send("phone:toggleLike", tweetId);
-    setLiked(!liked);
+    setLoading(true);
+    window.setTimeout(() => {
+      setLiked(!liked);
+      setLoading(false);
+    }, LOADING_TIME);
   };
+
+  if (loading) {
+    return (
+      <Button disabled>
+        <CircularProgress size={22} />
+      </Button>
+    );
+  }
 
   return (
     <Button onClick={handleClick}>
