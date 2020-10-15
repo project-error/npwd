67dac1eef01ff58defd3e691fca6e7ab6fa4622a
chip <onlygamernerd@gmail.com>
2020-10-15 18:17:25 +0200

    Cleanup sv_contacts

--- resources/server/sv_contacts.ts
+++ resources/server/sv_contacts.ts
@@ -34,36 +34,6 @@ onNet(events.CONTACTS_GET_CONTACTS, async (callback: any) => {
 
 })
 
-//ESX.RegisterServerCallback(events.CONTACTS_GET_CONTACTS, (cb: any) => {
-//  const _source = (global as any).source
-//  const xPlayer = ESX.GetPlayerFromId(_source);
-//  const _identifier = xPlayer.getIdentifier();
-//  
-//  const contacts = fetchAllContacts(_identifier);
-//  //console.log(contacts);
-//  
-//  cb(contacts);
-//
-//  console.log("Contacts: ")
-//  console.log(contacts);
-//})
-
-//ESX.RegisterServerCallback('phone:getContacts', function(source, cb)
-//    local _source = source
-//    local xPlayer = ESX.GetPlayerFromId(_source)
-//    local _identifier = xPlayer.getIdentifier()
-//    MySQL.Async.fetchAll('SELECT * FROM npwd_phone_contacts WHERE //`identifier`=@identifier', 
-//    {
-//        ['@identifier'] = _identifier
-//    }, function(contacts)
-//        cb(contacts)
-//        print("Contacts:")
-//        for k,v in pairs(contacts) do
-//            print(v.number, v.display)
-//        end
-//    end)
-//end)
-//
 //RegisterServerEvent('phone:addContacts')
 //AddEventHandler('phone:addContacts', function(display, number)
 //    local _source = source
