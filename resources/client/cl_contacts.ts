a28df727f18b71f2ce76cd308326342f8ffb00a6
chip <onlygamernerd@gmail.com>
2020-10-15 18:11:11 +0200

    Feat(Contacts:client): Sending data to NUI

--- resources/client/cl_contacts.ts
+++ resources/client/cl_contacts.ts
@@ -1,17 +1,16 @@
 import { ESX } from '../client/client';
 import events from '../utils/events';
 
-onNet(events.CONTACTS_SEND_CONTACTS, () => {
-  ESX.TriggerServerCallback(events.CONTACTS_GET_CONTACTS, (contacts: any) => {
-    SendNuiMessage(
-      JSON.stringify({
-        app: 'CONTACTS',
-        method: 'setContacts',
-        data: contacts
-      })
-    )
-  })
-});
+
+onNet(events.CONTACTS_SEND_CONTACTS, (contacts: any) => {
+  SendNuiMessage(
+    JSON.stringify({
+      app: 'CONTACTS',
+      method: 'setContacts',
+      data: contacts
+    })
+  )
+})
 
 RegisterNuiCallbackType(events.CONTACTS_ADD_CONTACT_NUI);
 on(`__cfx_nui:${events.CONTACTS_ADD_CONTACT_NUI}`, (data: any) => {
