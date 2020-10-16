d329eb6a5b95d3808897f043a2b520c416cf8497
chip <onlygamernerd@gmail.com>
2020-10-16 18:31:48 +0200

    Feat(Contacts): New hook for showing modal

---
+++ phone/src/apps/contacts/hooks/useModal.ts
@@ -0,0 +1,7 @@
+import { useRecoilState } from 'recoil';
+import { contactsState } from './state';
+
+export const useModal = () => {
+  const [ showModal, setShowModal ] = useRecoilState(contactsState.showModal);
+  return { showModal, setShowModal };
+}
