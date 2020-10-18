c68b57bead3918ab81afd1881ad80df66a55e4fd
chip <onlygamernerd@gmail.com>
2020-10-18 23:28:00 +0200

    New calling functions

--- resources/client/cl_call.lua
+++ resources/client/cl_call.lua
@@ -7,67 +7,13 @@ Citizen.CreateThread(function()
   end
 end)
 
------
---END OF ESX 
------
-local isPhoneOpen = false 
-local inCall = false
 RegisterNUICallback('phone:startCall', function(data)
-    print('Start call with ' .. data.number)
-    local phoneNumber = data.number
-    startCall(phoneNumber)
+  local phoneNumber = data.number
+  TriggerServerEvent('phone:beginCall', phoneNumber)
 end)
 
---[[RegisterNetEvent("phone:acceptCall")
-AddEventHandler("phone:acceptCall", function(phoneNumber, initiator)
-  print("should accept")
-  if inCall == false then
-    inCall = true
-    
-    exports["mumble-voip"]:SetCallChannel(infoCall.id+1)
-  end
-  if isPhoneOpen == false then 
-    isPhoneOpen = true
-    phoneOpenAnim()
-    SetCursorLocation(0.936, 0.922) -- Experimental
-    local res = GetActiveScreenResolution()
-    --print(res)
-    SendNUIMessage( -- Shows phone
-        {
-        app = 'PHONE',
-        method = 'setVisibility',
-        data = true
-        }
-    )
-    SetNuiFocus(true, true)
-    TriggerServerEvent('phone:getCredentials', source)
-  end
-end)--]]
-
-RegisterCommand('answer', function(source) -- Toggles Phone
-  local rocko = 2222222
-  local chip = 1111111
-  acceptCall(rocko, chip)
-  print(rocko)
-end, false)
-
-function startCall(transmitter, target)
-  TriggerServerEvent('phone:startCall', transmitter, target)
-end
-
-function acceptCall (phoneNumber)
-  TriggerServerEvent('phone:acceptCall', phoneNumber)
-  print(phoneNumber)
-end
-
-function rejectCall(phoneNumber)
-  TriggerServerEvent('phone:rejectCall', infoCall)
-end
-
-RegisterNetEvent('phone:waitingCall') -- Done for caller/ reciever
-AddEventHandler('phone:waitingCall', function(phoneNumber)
-  local playerPed = PlayerPedId()
-  --print(phoneNumber)
-  print("oi")
-  --TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.5, 'cellCall', 0.4)
+RegisterNetEvent('phone:startCall')
+AddEventHandler('phone:startCall', function(partner, phoneNumber, id)
+  print('You are talking to ' .. partner .. ', who has the number: ' .. phoneNumber);
+  exports["mumble-voip"]:SetCallChannel(id+1)
 end)
--- resources/server/sv_call.lua
+++ resources/server/sv_call.lua
@@ -1,31 +1,30 @@
 ESX = nil
-db = DatabaseConfig  -- helper variable for use in server functions
 
 TriggerEvent(
-    "esx:getSharedObject",
-    function(obj)
-        ESX = obj
-    end
+  "esx:getSharedObject",
+  function(obj)
+      ESX = obj
+  end
 )
 
 function getIdentifierByNumber(phoneNumber) 
-    local result = MySQL.Sync.fetchAll("SELECT " .. db.users.id .. " AS identifier FROM " .. db.users.table .. " WHERE " .. db.users.phoneNumber .. " = @phone_number", {
-        ['@phone_number'] = phoneNumber
-    })
-    if result[1] ~= nil then
-        return result[1].identifier
-    end
-    return nil
+  local result = MySQL.Sync.fetchAll("SELECT identifier FROM users WHERE phone_number = @phone_number", {
+    ['@phone_number'] = phoneNumber
+  })
+  if result[1] ~= nil then
+      return result[1].identifier
+  end
+  return nil
 end
 
 function getPhoneNumber(identifier) 
-    local result = MySQL.Sync.fetchAll("SELECT " .. db.users.phoneNumber .. " AS phone_number FROM " .. db.users.table .. " WHERE " .. db.users.id .. " = @identifier", {
-        ['@identifier'] = identifier
-    })
-    if result[1] ~= nil then
-        return result[1].phone_number
-    end
-    return nil
+  local result = MySQL.Sync.fetchAll("SELECT users.phone_number FROM users WHERE users.identifier = @identifier", {
+    ['@identifier'] = identifier
+  })
+  if result[1] ~= nil then
+      return result[1].phone_number
+  end
+  return nil
 end
 
 function getSourceFromIdentifier(identifier, cb)
@@ -46,58 +45,20 @@ function getPlayerID(source)
     return player
 end
 
-function getIdentifiant(id)
-    for _, v in ipairs(id) do
-        return v
-    end
-end
+RegisterServerEvent('phone:beginCall')
+AddEventHandler('phone:beginCall', function(phoneNumber)
+  local _source = source
+  local xPlayer = ESX.GetPlayerFromId(_source)
+  local _identifier = xPlayer.getIdentifier()
+  local callerName = xPlayer.getName()
+  local callerNumber = getPhoneNumber(_identifier);
 
-RegisterServerEvent('phone:startCall')
-AddEventHandler('phone:startCall', function(phoneNumber)
-    local _source = source
-    TriggerEvent('phone:initalizeCall', _source, phoneNumber)
+  
+  local targetIdentifier = getIdentifierByNumber(phoneNumber)
+  getSourceFromIdentifier(targetIdentifier, function(target)
+    local xTarget = ESX.GetPlayerFromId(target)
+    local targetName = xTarget.getName()
+    TriggerClientEvent('phone:startCall', target, callerName, callerNumber, _source)
+    TriggerClientEvent('phone:startCall', _source, targetName, phoneNumber, target)
+  end)
 end)
-
---[[RegisterServerEvent('phone:initalizeCall')
-AddEventHandler('phone:initalizeCall', function(phoneNumber)
-    local sourcePlayer = tonumber(source)
-    local zPlayer = getIdentifierByNumber(phoneNumber)
-    if zPlayer ~= nil then
-        getSourceFromIdentifier(zPlayer, function(target)
-            if tonumber(target) ~= nil then
-                print('Target: ' .. target)
-            end
-        end)
-    end
-end)]]
-
-RegisterServerEvent('phone:initalizeCall')
-AddEventHandler('phone:initalizeCall', function(source, phoneNumber)
-    if phoneNumber == nil or phoneNumber == '' then 
-        print('Number not Valid')
-        return
-    end
-
-    local sourcePlayer = tonumber(source)
-    local srcIdentifier = getPlayerID(source)
-
-    local srcPhone = ''
-
-    local zPlayer = getIdentifierByNumber(phoneNumber)
-    local is_valid = zPlayer ~= nil and zPlayer ~= srcIdentifier
-    
-    if is_valid == true then
-        getSourceFromIdentifier(zPlayer, function (target)
-            if target ~= nil then
-                TriggerClientEvent('phone:waitingCall', sourcePlayer, phoneNumber, true) -- Is calling
-                TriggerClientEvent('phone:waitingCall', target, phoneNumber, false) -- Calling
-            else
-                TriggerClientEvent('phone:waitingCall', sourcePlayer, phoneNumber, true)
-            end
-        end)
-    else
-        TriggerClientEvent('phone:waitingCall', sourcePlayer, phoneNumber, true)
-    end
-end)
-
-
