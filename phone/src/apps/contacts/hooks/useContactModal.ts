4905de53d28179e4cc76edeb47d4f7cd58a19065
chip <onlygamernerd@gmail.com>
2020-10-18 21:44:33 +0200

    Feat(Contacts:UI): Dropdown menu for updating contacts

---
+++ phone/src/apps/contacts/hooks/useContactModal.ts
@@ -0,0 +1,7 @@
+import { useRecoilState } from 'recoil';
+import { contactsState } from './state';
+
+export const useContactModal = () => {
+  const [ showContactModal, setShowContactModal ] = useRecoilState(contactsState.showContactsModal);
+  return { showContactModal, setShowContactModal };
+}
