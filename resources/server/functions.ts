0d966a0e10e5a03fa3b78a47363099076719dd3e
chip <onlygamernerd@gmail.com>
2020-10-20 00:48:16 +0200

    feat(functions:server): New hook for getting phone number from identifier | not done

--- resources/server/functions.ts
+++ resources/server/functions.ts
@@ -1,9 +1,7 @@
 import  { pool } from './db';
-import { ESX, getSource } from './server';
 
 export const usePhoneNumber = async (identifier: string) => {
   const query = "SELECT phone_number FROM users WHERE identifier = ?"
-  const result = await pool.query(query, [identifier]);
-
-  return result
+  const phoneNumber = await pool.query(query, [identifier]);
+  return phoneNumber;
 }
