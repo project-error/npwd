04a9297e4ec207df878509db88a552da7bb6d3a2
chip <onlygamernerd@gmail.com>
2020-10-20 15:47:41 +0200

    Moved transactions

--- phone/src/apps/bank/components/transactions/TransactionList.tsx
+++ phone/src/apps/bank/components/transactions/TransactionList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { List } from "../../../ui/components/List";
-import { ListItem } from "../../../ui/components/ListItem";
+import { List } from "../../../../ui/components/List";
+import { ListItem } from "../../../../ui/components/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
 export const TransactionList = ({ transactions }) => {
