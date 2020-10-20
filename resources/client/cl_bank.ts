097524d05476fe91797109f9127c05d81cdb5f27
chip <onlygamernerd@gmail.com>
2020-10-20 18:30:52 +0200

    feat(bank/clinet): client functionality

---
+++ resources/client/cl_bank.ts
@@ -0,0 +1,22 @@
+import events from '../utils/events';
+
+
+onNet(events.BANK_SEND_TRANSFERS, (transfer: any) => {
+  SendNuiMessage(
+    JSON.stringify({
+      app: "BANK",
+      method: "setTransaction",
+      data: transfer
+    })
+  )
+})
+
+RegisterNuiCallbackType(events.BANK_ADD_TRANSFER);
+on(`__cfx_nui:${events.BANK_ADD_TRANSFER}`, (data: any) => {
+  const transfer = data;
+  emitNet(events.BANK_ADD_TRANSFER, transfer)
+
+  setTimeout(() => {
+    emitNet(events.BANK_FETCH_TRANSACTIONS);
+  }, 500)
+})
