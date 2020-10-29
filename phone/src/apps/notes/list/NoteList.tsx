53ef7d019570f3ea592ca9897bcc0dad7d99d75c
chip <onlygamernerd@gmail.com>
2020-10-30 00:19:34 +0100

    fix(notes/nui): sending data from NoteList to the modal

--- phone/src/apps/notes/list/NoteList.tsx
+++ phone/src/apps/notes/list/NoteList.tsx
@@ -1,13 +1,27 @@
-import { List } from '@material-ui/core';
-import React from 'react'
-import { NoteItem } from './NoteItem';
+import { List, ListItem, ListItemText } from '@material-ui/core';
+import React, { useState } from 'react'
+
+import { useNoteModal } from '../hooks/useNoteModal';
+import { useNoteDetail } from '../hooks/useNoteDetail';
 
 // add search bar later
 const NoteList = ({notes}) => {
+
+  const { noteModal, setNoteModal } = useNoteModal();
+  const { detail, setDetail } = useNoteDetail();
+
+  const handleNoteModal = (note) => {
+    setNoteModal(true)
+    setDetail(note)
+    console.log(note.id)
+  }
+
   return (
     <List>
       {notes.map((note) => (
-        <NoteItem key={note.id} {...note} />
+        <ListItem key={note.id} button divider onClick={() => handleNoteModal(note)}>
+          <ListItemText><strong>{note.title}</strong></ListItemText>
+        </ListItem>
       ))}
     </List>
   )
