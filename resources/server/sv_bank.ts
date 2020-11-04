66655f40f6c06a85021d0d81a5bdccea24a43f92
chip <onlygamernerd@gmail.com>
2020-11-04 16:16:53 +0100

    feat(bank/server): able to get current transfer | checks for transferring | (needs clean up)

--- resources/server/sv_bank.ts
+++ resources/server/sv_bank.ts
@@ -29,32 +29,45 @@ function fetchCredentials(): Credentials {
 async function addTransfer(identifier: string, transfer: Transfer): Promise<any> {
   const xPlayer = ESX.GetPlayerFromId(getSource())
   const bankBalance = xPlayer.getAccount('bank').money;
+  const sourceName = xPlayer.getName()
 
-
-  const xTarget = ESX.GetPlayerFromId(transfer.targetID)
-
-  console.log(bankBalance)
   if (transfer.amount > bankBalance) {
     console.log("poor man")
-    emitNet(events.BANK_TRANSACTION_ALERT, getSource, 'You do not have enough money on your account', 'error')
+    emitNet(events.BANK_TRANSACTION_ALERT, getSource(), false)
   } 
-  else if (transfer.amount <= bankBalance) {
-    const query = "INSERT INTO npwd_bank_transfers (identifier, target, amount, message) VALUES (?, ?, ?, ?)"
+  else if (transfer.amount < bankBalance) {
+    emitNet(events.BANK_TRANSACTION_ALERT, getSource(), true)
+
+
+    const query = "INSERT INTO npwd_bank_transfers (identifier, target, amount, message, type, source) VALUES (?, ?, ?, ?, ?, ?)"
     // emit alert to target iD
+    console.log("rich man")
   
-    await pool.query(query, [
+    const [results] = await pool.query(query, [
       identifier, 
       transfer.targetID,
       transfer.amount,
-      transfer.message
+      transfer.message,
+      "Deposit",
+      sourceName,
     ])
-
-    emitNet(events.BANK_TRANSACTION_ALERT, getSource, 'You succesfully transferred money to someone', 'success')
-  
-    xTarget.addAccountMoney('bank', transfer.amount)
-
-
+    const insertData = <any>results;
+    console.log("ID: ", insertData.insertId)
+    return await getTransfer(insertData.insertId)
+    //xTarget.addAccountMoney('bank', transfer.amount)
   }
+}
+
+/**
+ * Gets a transfer from its ID
+ * @param transferId - transfer id
+ */
+async function getTransfer(transferId: number): Promise<Transfer> {
+  const query = "SELECT * FROM npwd_bank_transfers WHERE id = ?"
+  const [ results ] = await pool.query(query, [transferId]);
+  const transfers = <Transfer[]>results;
+  const transfer = transfers[0];
+  return {...transfer};
 }
 
 onNet(events.BANK_FETCH_TRANSACTIONS, async () => {
@@ -64,17 +77,18 @@ onNet(events.BANK_FETCH_TRANSACTIONS, async () => {
     const transfer = await fetchAllTransactions(_identifier);
 
     emitNet(events.BANK_SEND_TRANSFERS, _source, transfer)
-    console.log("WOORK")
 
   } catch (error) {
     console.log('Failed to fetch transactions', error)
   }
 })
 
-onNet(events.BANK_ADD_TRANSFER, (transfer: Transfer) => {
+onNet(events.BANK_ADD_TRANSFER, async (transfer: Transfer) => {
   try {
     const _identifier = ESX.GetPlayerFromId(getSource()).getIdentifier()
-    addTransfer(_identifier, transfer)
+    const transferNotify = await addTransfer(_identifier, transfer)
+    console.log(transferNotify.id)
+    emitNet(events.BANK_TRANSACTION_NOTIFICATION, getSource(), transferNotify)
   } catch (error) {
     console.log('Failed to add transfer', error)
   }
