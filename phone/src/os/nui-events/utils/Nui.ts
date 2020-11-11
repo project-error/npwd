b37e72d168e547197b8bec4ed5cba3fea4cd0df0
chip <onlygamernerd@gmail.com>
2020-11-11 14:49:06 +0100

    fucked up the nui fetch

--- phone/src/os/nui-events/utils/Nui.ts
+++ phone/src/os/nui-events/utils/Nui.ts
@@ -2,7 +2,7 @@
 export default {
   async send(event: any, data = {}) {
     /* eslint-disable no-unreachable */
-    return fetch(`http://or-ambumdt/${event}`, {
+    return fetch(`http://new-phone-who-dis/${event}`, {
       method: "post",
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
