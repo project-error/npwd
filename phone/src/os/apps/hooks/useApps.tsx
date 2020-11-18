07ff8833c731de447aefe6074763f1531afc637e
Taso <michael@mckeen.us>
2020-11-18 18:21:40 -0500

    styles(theming): Started a new custom dark theme - Default has been changed to taso-dark while in-development

--- phone/src/os/apps/hooks/useApps.tsx
+++ phone/src/os/apps/hooks/useApps.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useConfig } from "../../../config/hooks/useConfig";
+import React from 'react';
+import { useConfig } from '../../../config/hooks/useConfig';
 import {
   blue,
   common,
@@ -9,109 +9,109 @@ import {
   yellow,
   red,
   amber,
-} from "@material-ui/core/colors";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+} from '@material-ui/core/colors';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faAd,
   faPlaneArrival,
   faPhoneAlt,
   faWonSign,
-  faStickyNote
-} from "@fortawesome/free-solid-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+  faStickyNote,
+} from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-import ContactsIcon from "@material-ui/icons/Contacts";
-import SettingsIcon from "@material-ui/icons/Settings";
-import MessageIcon from "@material-ui/icons/Message";
-import { atom } from "recoil";
-import { CalculatorIcon } from "../../../apps/calculator/components/CalculatorIcon";
-import { Route } from "react-router-dom";
-import { DialerApp } from "../../../apps/dialer/components/DialerApp";
-import { ContactsApp } from "../../../apps/contacts/components/ContactsApp";
-import { CalculatorApp } from "../../../apps/calculator/components/CalculatorApp";
-import { SettingsApp } from "../../../apps/settings/components/SettingsApp";
-import { BankApp } from "../../../apps/bank/components/BankApp";
-import { MessagesApp } from "../../../apps/messages/components/MessagesApp";
-import { TwitterApp } from "../../../apps/twitter/components/TwitterApp";
-import { ExampleApp } from "../../../apps/example/components/ExampleApp";
-import { SelloutApp } from "../../../apps/sellout/components/SelloutApp";
-import { NotesApp } from "../../../apps/notes/NotesApp";
+import ContactsIcon from '@material-ui/icons/Contacts';
+import SettingsIcon from '@material-ui/icons/Settings';
+import MessageIcon from '@material-ui/icons/Message';
+import { atom } from 'recoil';
+import { CalculatorIcon } from '../../../apps/calculator/components/CalculatorIcon';
+import { Route } from 'react-router-dom';
+import { DialerApp } from '../../../apps/dialer/components/DialerApp';
+import { ContactsApp } from '../../../apps/contacts/components/ContactsApp';
+import { CalculatorApp } from '../../../apps/calculator/components/CalculatorApp';
+import { SettingsApp } from '../../../apps/settings/components/SettingsApp';
+import { BankApp } from '../../../apps/bank/components/BankApp';
+import { MessagesApp } from '../../../apps/messages/components/MessagesApp';
+import { TwitterApp } from '../../../apps/twitter/components/TwitterApp';
+import { ExampleApp } from '../../../apps/example/components/ExampleApp';
+import { SelloutApp } from '../../../apps/sellout/components/SelloutApp';
+import { NotesApp } from '../../../apps/notes/NotesApp';
 
 const appsState = atom({
-  key: "apps",
+  key: 'apps',
   default: {
     preinstalled: [
       {
-        id: "DIALER",
-        nameLocale: "APPS_DIALER",
-        icon: <FontAwesomeIcon icon={faPhoneAlt} size="xs" />,
+        id: 'DIALER',
+        nameLocale: 'APPS_DIALER',
+        icon: <FontAwesomeIcon icon={faPhoneAlt} size='xs' />,
         backgroundColor: green[600],
         color: common.white,
-        path: "/phone",
-        Route: () => <Route path="/phone" component={DialerApp} />,
+        path: '/phone',
+        Route: () => <Route path='/phone' component={DialerApp} />,
       },
       {
-        id: "MESSAGES",
-        nameLocale: "APPS_MESSAGES",
-        icon: <MessageIcon fontSize="default" />,
+        id: 'MESSAGES',
+        nameLocale: 'APPS_MESSAGES',
+        icon: <MessageIcon fontSize='default' />,
         backgroundColor: amber[700],
         color: common.white,
-        path: "/messages",
-        Route: () => <Route path="/messages" component={MessagesApp} />,
+        path: '/messages',
+        Route: () => <Route path='/messages' component={MessagesApp} />,
       },
       {
-        id: "CONTACTS",
-        nameLocale: "APPS_CONTACTS",
+        id: 'CONTACTS',
+        nameLocale: 'APPS_CONTACTS',
         icon: <ContactsIcon />,
         backgroundColor: blue[500],
         color: common.white,
-        path: "/contacts",
-        Route: () => <Route path="/contacts" component={ContactsApp} />,
+        path: '/contacts',
+        Route: () => <Route path='/contacts' component={ContactsApp} />,
       },
       {
-        id: "CALCULATOR",
-        nameLocale: "APPS_CALCULATOR",
+        id: 'CALCULATOR',
+        nameLocale: 'APPS_CALCULATOR',
         icon: <CalculatorIcon />,
         backgroundColor: purple[500],
         color: grey[50],
-        path: "/calculator",
-        Route: () => <Route path="/calculator" component={CalculatorApp} />,
+        path: '/calculator',
+        Route: () => <Route path='/calculator' component={CalculatorApp} />,
       },
       {
-        id: "SETTINGS",
-        nameLocale: "APPS_SETTINGS",
+        id: 'SETTINGS',
+        nameLocale: 'APPS_SETTINGS',
         icon: <SettingsIcon />,
-        backgroundColor: grey[700],
+        backgroundColor: '#383838',
         color: grey[50],
-        path: "/settings",
-        Route: () => <Route path="/settings" component={SettingsApp} />,
+        path: '/settings',
+        Route: () => <Route path='/settings' component={SettingsApp} />,
       },
       {
-        id: "BANK",
-        nameLocale: "APPS_BANK",
-        icon: <FontAwesomeIcon icon={faWonSign} size="xs" />,
+        id: 'BANK',
+        nameLocale: 'APPS_BANK',
+        icon: <FontAwesomeIcon icon={faWonSign} size='xs' />,
         backgroundColor: blue[900],
         color: common.white,
-        path: "/bank",
-        Route: () => <Route path="/bank" component={BankApp} />,
+        path: '/bank',
+        Route: () => <Route path='/bank' component={BankApp} />,
       },
       {
-        id: "TWITTER",
-        nameLocale: "APPS_TWITTER",
-        icon: <FontAwesomeIcon icon={faTwitter} fixedWidth size="xs" />,
+        id: 'TWITTER',
+        nameLocale: 'APPS_TWITTER',
+        icon: <FontAwesomeIcon icon={faTwitter} fixedWidth size='xs' />,
         backgroundColor: blue[600],
         color: common.white,
-        path: "/twitter",
-        Route: () => <Route path="/twitter" component={TwitterApp} />,
+        path: '/twitter',
+        Route: () => <Route path='/twitter' component={TwitterApp} />,
       },
       {
-        id: "SELLOUT",
-        nameLocale: "APPS_SELLOUT",
+        id: 'SELLOUT',
+        nameLocale: 'APPS_SELLOUT',
         icon: <FontAwesomeIcon icon={faAd} fixedWidth />,
         backgroundColor: red[500],
         color: common.white,
-        path: "/sellout",
-        Route: () => <Route path="/sellout" component={SelloutApp} />,
+        path: '/sellout',
+        Route: () => <Route path='/sellout' component={SelloutApp} />,
       },
       {
         id: 'NOTES',
@@ -119,17 +119,17 @@ const appsState = atom({
         icon: <FontAwesomeIcon icon={faStickyNote} fixedWidth />,
         backgroundColor: yellow[800],
         color: common.white,
-        path: "/notes",
-        Route: () => <Route path="/notes" component={NotesApp}/>
+        path: '/notes',
+        Route: () => <Route path='/notes' component={NotesApp} />,
       },
       {
-        id: "EXAMPLE",
-        nameLocale: "APPS_EXAMPLE",
-        icon: <FontAwesomeIcon icon={faPlaneArrival} size="sm" />,
+        id: 'EXAMPLE',
+        nameLocale: 'APPS_EXAMPLE',
+        icon: <FontAwesomeIcon icon={faPlaneArrival} size='sm' />,
         backgroundColor: blue[500],
         color: blue[50],
-        path: "/example",
-        Route: () => <Route path="/example" component={ExampleApp} />,
+        path: '/example',
+        Route: () => <Route path='/example' component={ExampleApp} />,
       },
     ],
   },
