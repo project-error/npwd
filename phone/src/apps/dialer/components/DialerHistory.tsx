cb25aa9324be911c6e7b2f4f0e9f327983b2d5a0
Taso <michael@mckeen.us>
2020-11-18 21:16:09 -0500

    styling(Lists): disbled padding on several list elements

--- phone/src/apps/dialer/components/DialerHistory.tsx
+++ phone/src/apps/dialer/components/DialerHistory.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ListItemText from "@material-ui/core/ListItemText";
-import PhoneCallbackIcon from "@material-ui/icons/PhoneCallback";
-import PhoneForwardedIcon from "@material-ui/icons/PhoneForwarded";
-import { List } from "../../../ui/components/List";
-import { ListItem } from "../../../ui/components/ListItem";
+import React from 'react';
+import ListItemText from '@material-ui/core/ListItemText';
+import PhoneCallbackIcon from '@material-ui/icons/PhoneCallback';
+import PhoneForwardedIcon from '@material-ui/icons/PhoneForwarded';
+import { List } from '../../../ui/components/List';
+import { ListItem } from '../../../ui/components/ListItem';
 
 const CallTypeIcon = {
   incoming: <PhoneCallbackIcon />,
@@ -12,7 +12,7 @@ const CallTypeIcon = {
 
 export const DialerHistory = ({ calls }) => {
   return (
-    <List>
+    <List disablePadding>
       {calls.map((call) => (
         <ListItem key={call.id} divider>
           <ListItemText primary={call.caller} secondary={call.phoneNumber} />
--- phone/src/apps/settings/components/SettingsApp.tsx
+++ phone/src/apps/settings/components/SettingsApp.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { AppWrapper } from "../../../ui/components";
-import { AppTitle } from "../../../ui/components/AppTitle";
-import { AppContent } from "../../../ui/components/AppContent";
+import React from 'react';
+import { AppWrapper } from '../../../ui/components';
+import { AppTitle } from '../../../ui/components/AppTitle';
+import { AppContent } from '../../../ui/components/AppContent';
 import {
   useContextMenu,
   MapStringOptions,
-} from "../../../ui/hooks/useContextMenu";
-import { useConfig } from "../../../config/hooks/useConfig";
-import { useSettings } from "../hooks/useSettings";
-import { List } from "../../../ui/components/List";
-import { useSimcard } from "../../../os/simcard/hooks/useSimcard";
-import { useApp } from "../../../os/apps/hooks/useApps";
-import { SettingItem } from "./SettingItem";
+} from '../../../ui/hooks/useContextMenu';
+import { useConfig } from '../../../config/hooks/useConfig';
+import { useSettings } from '../hooks/useSettings';
+import { List } from '../../../ui/components/List';
+import { useSimcard } from '../../../os/simcard/hooks/useSimcard';
+import { useApp } from '../../../os/apps/hooks/useApps';
+import { SettingItem } from './SettingItem';
 
 export const SettingsApp = () => {
-  const settingsApp = useApp("SETTINGS");
+  const settingsApp = useApp('SETTINGS');
   const [config] = useConfig();
   const { setSettings, settings } = useSettings();
   const simcard = useSimcard();
 
   const wallpapers = config.wallpapers.map(
-    MapStringOptions(settings.wallpaper, (val) => setSettings("wallpaper", val))
+    MapStringOptions(settings.wallpaper, (val) => setSettings('wallpaper', val))
   );
   const frames = config.frames.map(
-    MapStringOptions(settings.frame, (val) => setSettings("frame", val))
+    MapStringOptions(settings.frame, (val) => setSettings('frame', val))
   );
   const themes = Object.keys(config.themes).map(
-    MapStringOptions(settings.theme, (val) => setSettings("theme", val))
+    MapStringOptions(settings.theme, (val) => setSettings('theme', val))
   );
 
   const [openMenu, closeMenu, ContextMenu, isMenuOpen] = useContextMenu();
@@ -34,26 +34,26 @@ export const SettingsApp = () => {
     <AppWrapper>
       <AppTitle app={settingsApp} />
       <AppContent backdrop={isMenuOpen} onClickBackdrop={closeMenu}>
-        <List>
+        <List disablePadding>
           <SettingItem
-            label="Theme"
+            label='Theme'
             value={settings.theme}
             options={themes}
             onClick={openMenu}
           />
           <SettingItem
-            label="Wallpaper"
+            label='Wallpaper'
             value={settings.wallpaper}
             options={wallpapers}
             onClick={openMenu}
           />
           <SettingItem
-            label="Frame"
+            label='Frame'
             value={settings.frame}
             options={frames}
             onClick={openMenu}
           />
-          <SettingItem label="Phone Number" value={simcard.number} />
+          <SettingItem label='Phone Number' value={simcard.number} />
         </List>
       </AppContent>
       <ContextMenu />
