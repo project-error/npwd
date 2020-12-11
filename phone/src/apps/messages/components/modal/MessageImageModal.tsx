1249c3a3640dac64079cc5409274e2c59d8ba746
Christopher <onlygamernerd@gmail.com>
2020-12-11 17:23:55 +0100

    feat(messages/nui): modal for sending images

---
+++ phone/src/apps/messages/components/modal/MessageImageModal.tsx
@@ -0,0 +1,51 @@
+import { TextField, Button } from '@material-ui/core';
+import React, { useState } from 'react'
+import Nui from '../../../../os/nui-events/utils/Nui';
+import Modal from '../../../../ui/components/Modal'
+import { useImageModal } from '../../hooks/useImageModal'
+import CloseIcon from '@material-ui/icons/Close';
+import useStyles from './modal.styles';
+
+interface IProps {
+  messageGroupId: string | undefined;
+}
+
+export const MessageImageModal = ({ messageGroupId }: IProps) => {
+  const classes = useStyles();
+  const { imageModal, setImageModal } = useImageModal();
+  const [ message, setMessage ] = useState("");
+
+  const _handleClose = () => {
+    setImageModal(false)
+  }
+
+  const sendImageMessage = (event) => {
+    if (event.key ==="Enter") {
+      Nui.send('phone:sendMessage', {
+        groupId: messageGroupId,
+        message
+      })
+      setImageModal(false);
+    }
+  }
+
+
+  return (
+    <Modal visible={imageModal} handleClose={_handleClose}>
+      <div>
+        <Button onClick={_handleClose} className={classes.imageModalCloseButton} ><CloseIcon /></Button>
+        <TextField 
+          placeholder="A link to your image or gif"
+          onChange={e => setMessage(e.target.value)}
+          value={message}
+          className={classes.input}
+          inputProps={{
+            className: classes.messagesInput,
+          }}
+          inputRef={(input) => input && input.focus()}
+          onKeyPress={sendImageMessage}
+        />
+      </div>
+    </Modal>
+  )
+}
---
+++ phone/src/apps/messages/hooks/useImageModal.ts
@@ -0,0 +1,8 @@
+import { useRecoilState } from 'recoil';
+import { messageState } from './state';
+
+
+export const useImageModal = () => {
+  const [ imageModal, setImageModal ] = useRecoilState(messageState.imageModal);
+  return { imageModal, setImageModal };
+}
