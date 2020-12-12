b4a1ba0197b8c57d8ecbf20f9b362991153ca31f
Christopher <onlygamernerd@gmail.com>
2020-12-12 15:14:33 +0100

    tweak(bank/nui): spearate styling for account and card

---
+++ phone/src/apps/bank/components/account/card.styles.ts
@@ -0,0 +1,35 @@
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    background: '#388CF8',
+    height: '179px',
+    width: '350px',
+    margin: 'auto',
+    borderRadius: '15px',
+    fontFamily: 'Bahnschrift Regular',
+  },
+  cardName: {
+    margin: 10,
+    paddingTop: 15,
+    fontSize: '24px',
+  },
+  cardAccount: {
+    margin: '10px',
+    marginTop: '10px',
+    color: '#DFDFDF',
+    fontWeight: 'bold',
+    borderBottom: '2px solid #D8D1D1',
+    paddingBottom: 5,
+  },
+  balance: {
+    position: 'absolute',
+    bottom: '53%',
+    left: '10%',
+  },
+  balanceFont: {
+    fontSize: '29px',
+  },
+}));
+
+export default useStyles;
