b6d363fabd4bba1b36a5a1d1166cda6c683eede7
Taso <taso@reignited-dev.com>
2020-12-15 21:56:07 -0500

    Revert "refactor(import SQL): Misc. comments/changes still WIP "
    This reverts commit 3f1c8b890b6d2e9307e610625d7f2d9344bf35c1.

--- resources/import.sql
+++ resources/import.sql
@@ -1,8 +1,6 @@
 -- comment this out if your server has a different table/location for phone_number
 -- or if it already exists in the users table
-
-# This whole thing needs a lot more work, data structure can be heavily improved.
-
+ALTER TABLE `users` ADD COLUMN `phone_number` VARCHAR(20) NULL DEFAULT NULL;
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('phone', 'Phone', 1, 0, 1);
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('dphone', 'Destroyed Phone', 1, 0, 1);
 
@@ -17,35 +15,35 @@ CREATE TABLE IF NOT EXISTS npwd_phone_contacts (
 ENGINE = INNODB,
 AUTO_INCREMENT = 6,
 AVG_ROW_LENGTH = 4096,
-CHARACTER SET utf8,
-COLLATE utf8_bin;
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS npwd_twitter_tweets (
+CREATE TABLE IF NOT EXISTS `npwd_twitter_tweets` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `message` varchar(1000) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `message` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `likes` int NOT NULL DEFAULT '0',
-  `identifier` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `identifier` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `visible` tinyint NOT NULL DEFAULT '1',
-  `images` varchar(1000) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '',
+  `images` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=190 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=190 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE IF NOT EXISTS `npwd_twitter_profiles` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `profile_name` varchar(90) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `identifier` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `avatar_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `bio` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `location` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `job` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `profile_name` varchar(90) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `identifier` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `avatar_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `bio` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `location` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `job` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `profile_name_UNIQUE` (`profile_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `npwd_twitter_likes` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -57,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `npwd_twitter_likes` (
   KEY `tweet_idx` (`tweet_id`),
   CONSTRAINT `profile` FOREIGN KEY (`profile_id`) REFERENCES `npwd_twitter_profiles` (`id`),
   CONSTRAINT `tweet` FOREIGN KEY (`tweet_id`) REFERENCES `npwd_twitter_tweets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE IF NOT EXISTS npwd_notes (
@@ -70,7 +68,8 @@ CREATE TABLE IF NOT EXISTS npwd_notes (
 ENGINE = INNODB,
 AUTO_INCREMENT = 5,
 AVG_ROW_LENGTH = 4096,
-CHARACTER SET utf8;
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS npwd_sellout_listings (
   id int(11) NOT NULL AUTO_INCREMENT,
@@ -85,7 +84,8 @@ CREATE TABLE IF NOT EXISTS npwd_sellout_listings (
 ENGINE = INNODB,
 AUTO_INCREMENT = 26,
 AVG_ROW_LENGTH = 2048,
-CHARACTER SET utf8;
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `npwd_twitter_reports` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -97,68 +97,50 @@ CREATE TABLE IF NOT EXISTS `npwd_twitter_reports` (
   KEY `tweet_idx` (`tweet_id`),
   CONSTRAINT `report_profile` FOREIGN KEY (`profile_id`) REFERENCES `npwd_twitter_profiles` (`id`),
   CONSTRAINT `report_tweet` FOREIGN KEY (`tweet_id`) REFERENCES `npwd_twitter_tweets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-# CREATE TABLE IF NOT EXISTS `npwd_messages_groups` (
-#   `id` int NOT NULL AUTO_INCREMENT,
-#   `user_identifier` varchar(40) NOT NULL,
-#   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-#   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-#   `group_id` varchar(60) NOT NULL,
-#   `participant_identifier` varchar(40) NOT NULL,
-#   `label` varchar(60) DEFAULT '',
-#   PRIMARY KEY (`id`),
-#   KEY `npwd_messages_groups_group_id` (`group_id`),
-#   KEY `npwd_messages_groups_user_identifier_idx` (`user_identifier`),
-#   KEY `npwd_messages_groups_participant_identifier_idx` (`participant_identifier`),
-#   CONSTRAINT `npwd_messages_groups_participant_identifier` FOREIGN KEY (`participant_identifier`) REFERENCES `users` (`identifier`),
-#   CONSTRAINT `npwd_messages_groups_user_identifier` FOREIGN KEY (`user_identifier`) REFERENCES users(`identifier`)
-# ) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8;
-
-# TODO: FIX TABLE
-# This table needs some data work, it won't be possible in the way it was implemented
 CREATE TABLE IF NOT EXISTS `npwd_messages_groups` (
-    ` id ` INT(11) NOT NULL AUTO_INCREMENT,
-    ` user_identifier ` VARCHAR(40) NOT NULL,
-    ` createdAt ` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-    ` updatedAt ` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    ` group_id ` VARCHAR(60) NOT NULL,
-    ` label ` VARCHAR(60) NOT NULL,
-    ` participant_identifier` VARCHAR(40) NOT NULL,
-    PRIMARY KEY (` id `) USING BTREE,
-    INDEX ` npwd_messages_groups_group_id ` (` group_id `) USING BTREE,
-    INDEX ` npwd_messages_groups_user_identifier_idx ` (` user_identifier `) USING BTREE
-#     INDEX ` npwd_messages_groups_participant_identifier_idx` (`participant_identifier`) USING BTREE
-) COLLATE='utf8_general_ci' ENGINE=MyISAM AUTO_INCREMENT=66;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_identifier` varchar(40) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `group_id` varchar(60) NOT NULL,
+  `participant_identifier` varchar(40) NOT NULL,
+  `label` varchar(60) DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `npwd_messages_groups_group_id` (`group_id`),
+  KEY `npwd_messages_groups_user_identifier_idx` (`user_identifier`),
+  KEY `npwd_messages_groups_participant_identifier_idx` (`participant_identifier`),
+  CONSTRAINT `npwd_messages_groups_participant_identifier` FOREIGN KEY (`participant_identifier`) REFERENCES `users` (`identifier`),
+  CONSTRAINT `npwd_messages_groups_user_identifier` FOREIGN KEY (`user_identifier`) REFERENCES `users` (`identifier`)
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-
-CREATE TABLE `npwd_messages_labels` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `user_identifier` VARCHAR(40) NOT NULL DEFAULT '',
-    `label` VARCHAR(60) NOT NULL DEFAULT '',
-    `createdAt` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-    `updatedAt` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `groupId` VARCHAR(60) NOT NULL,
-    INDEX `npwd_messages_labels_group_id_idx1` (`user_identifier`),
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fk_groupId` FOREIGN KEY (`groupId`) REFERENCES `npwd_messages_groups` (`group_id`)
-) COLLATE='utf8_general_ci' ENGINE=InnoDB AUTO_INCREMENT=20;
-
+CREATE TABLE IF NOT EXISTS `npwd_messages_labels` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_identifier` varchar(40) NOT NULL,
+  `label` varchar(60) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `group_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `npwd_messages_labels_group_id_idx` (`group_id`),
+  KEY `npwd_messages_labels_group_id_idx1` (`user_identifier`),
+  CONSTRAINT `npwd_messages_labels_group_id` FOREIGN KEY (`group_id`) REFERENCES `npwd_messages_groups` (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `npwd_messages` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `message` varchar(512) NOT NULL,
-    `user_identifier` varchar(40) NOT NULL,
-    `group_id` varchar(60) NOT NULL,
-    `isRead` tinyint NOT NULL DEFAULT '0',
-    `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `visible` tinyint NOT NULL DEFAULT '1',
-    PRIMARY KEY (`id`),
-    KEY `messages_identifier_idx` (`user_identifier`),
-    KEY `npwd_messages_group_id_idx` (`group_id`),
-    CONSTRAINT `npwd_messages_group_id` FOREIGN KEY (`group_id`) REFERENCES `npwd_messages_groups` (`group_id`),
-    CONSTRAINT `npwd_messages_user_identifier` FOREIGN KEY (`user_identifier`) REFERENCES `users` (`identifier`)
-) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` varchar(512) NOT NULL,
+  `user_identifier` varchar(40) NOT NULL,
+  `group_id` varchar(60) NOT NULL,
+  `isRead` tinyint NOT NULL DEFAULT '0',
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `visible` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `messages_identifier_idx` (`user_identifier`),
+  KEY `npwd_messages_group_id_idx` (`group_id`),
+  CONSTRAINT `npwd_messages_group_id` FOREIGN KEY (`group_id`) REFERENCES `npwd_messages_groups` (`group_id`),
+  CONSTRAINT `npwd_messages_user_identifier` FOREIGN KEY (`user_identifier`) REFERENCES `users` (`identifier`)
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
