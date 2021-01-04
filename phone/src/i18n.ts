528cfa907a9363962ad5697f2b0880c9df6bf2d6
Christopher <onlygamernerd@gmail.com>
2021-01-04 19:28:41 +0100

    feat(translation): added fr and es

--- phone/src/i18n.ts
+++ phone/src/i18n.ts
@@ -1,16 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locale/en.json";
+import es from './locale/es.json'
+import fr from './locale/fr.json';
+import config from './config/default.json';
 
 const resources = {
   en,
+  es,
+  fr
 };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en",
+    lng: config.language,
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
