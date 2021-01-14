20e03e498179c6934ddb8ae9d9f85346b5637b85
Christopher <onlygamernerd@gmail.com>
2021-01-14 20:02:59 +0100

    feat(camera/nui): interface on useShareModal

--- phone/src/apps/camera/hooks/useShareModal.ts
+++ phone/src/apps/camera/hooks/useShareModal.ts
@@ -1,7 +1,12 @@
 import { useRecoilState } from 'recoil';
 import { photoState } from './state';
 
-export const useShareModal = () => {
+interface ShareModalProps {
+  shareModal: boolean;
+  setShareModal: (show: boolean) => void;
+}
+
+export const useShareModal = (): ShareModalProps => {
   const [ shareModal, setShareModal ] = useRecoilState(photoState.shareModal);
   return { shareModal, setShareModal }
 }
