01291abe73e79540e7da6be8c02ec4936bb162ff
Christopher <onlygamernerd@gmail.com>
2021-01-14 18:58:50 +0100

    feat(contacts/nui): interface for useModal

--- phone/src/apps/contacts/hooks/useModal.ts
+++ phone/src/apps/contacts/hooks/useModal.ts
@@ -1,6 +1,11 @@
 import { useRecoilState } from "recoil";
 import { contactsState } from "./state";
 
+interface ModalProps {
+  showModal: boolean;
+  setShowModal: (show: boolean) => void;
+}
+
 export const useModal = () => {
   const [showModal, setShowModal] = useRecoilState(contactsState.showModal);
   return { showModal, setShowModal };
