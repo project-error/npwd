8a2f281790bbae222c2b9ec7109b61d90b49dbd6
Taso <taso@projecterror.dev>
2021-01-17 15:49:22 -0500

    enhance(phone): useNuiEvent types and integrated DebugLogger

--- phone/src/os/nui-events/hooks/useNuiEvent.ts
+++ phone/src/os/nui-events/hooks/useNuiEvent.ts
@@ -1,5 +1,6 @@
-import { useEffect, useRef } from 'react';
+import { MutableRefObject, useEffect, useRef } from 'react';
 import { eventNameFactory } from '../utils/nuiUtils';
+import DebugLog from '../../debug/LogDebugEvents';
 
 interface IOptions {
   capture?: boolean;
@@ -7,30 +8,48 @@ interface IOptions {
   once?: boolean;
 }
 
+/**
+ * A hook that manage events listeners for receiving data from the NUI
+ * @param app The app name in which this hoook is used
+ * @param method The specific `method` field that should be listened for.
+ * @param handler The callback function that will handle data relayed by this hook
+ * @param options Any options to pass to the addEventListener
+ **/
+
 export const useNuiEvent = (
-  app: any,
-  method: any,
-  handler: any,
+  app: string,
+  method: string,
+  handler: Function,
   options: IOptions = {}
 ) => {
-  const savedHandler: any = useRef();
+  const savedHandler: MutableRefObject<any> = useRef();
+  // Destructure passed options
   const { capture, passive, once } = options;
 
+  // When handler value changes set mutable ref to handler val
   useEffect(() => {
     savedHandler.current = handler;
   }, [handler]);
 
+  // Will run every rerender
   useEffect(() => {
     const eventName = eventNameFactory(app, method);
-    const eventListener = (event: any) => {
+    const eventListener = (event) => {
       if (savedHandler.current && savedHandler.current.call) {
         const { data } = event;
-        console.log(eventName, data);
+        DebugLog({
+          action: `NUI Data Received (${eventName})`,
+          data: event.data,
+          level: 1,
+        });
         savedHandler.current(data);
       }
     };
+    // Why are destructing then restructuring option data?
     const opts = { capture, passive, once };
+
     window.addEventListener(eventName, eventListener, opts);
+    // Remove Event Listener on component cleanup
     return () => window.removeEventListener(eventName, eventListener, opts);
   }, [app, method, capture, passive, once]);
 };
