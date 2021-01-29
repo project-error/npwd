f960d1414c3c1e349d3f549d6526c0c367dea5e0
Christopher <onlygamernerd@gmail.com>
2021-01-29 19:54:17 +0100

    feat(calling/client|server): show dail history

--- resources/server/sv_call.ts
+++ resources/server/sv_call.ts
@@ -1,9 +1,11 @@
 import { ESX } from './server';
 import events from '../utils/events';
-import { getIdentifierByPhoneNumber, usePhoneNumber } from './functions';
+import { getIdentifierByPhoneNumber, usePhoneNumber, useIdentifier} from './functions';
 import { XPlayer } from 'esx.js/@types/server';
-import { ICall } from '../../phone/src/common/typings/call'
-import { constants } from 'buffer';
+import { ICall, ICallUI } from '../../phone/src/common/typings/call'
+
+import { pool } from './db';
+import { reverse } from 'dns';
 
 /**
  * Returns the player phoneNumber for a passed identifier
@@ -27,7 +29,26 @@ async function getPlayerFromIdentifier(identifier: string): Promise<XPlayer> {
   });
 }
 
+
+async function saveCall(call: ICall, isAccepted: boolean) {
+  const query = "INSERT INTO npwd_calls (transmitter, receiver, accepted, is_caller) VALUES (?, ?)"
+  await pool.query(query, [call.transmitter, call.receiver, isAccepted])
+}
+
+async function updateCall(call: ICall, isAccepted: boolean) {
+  const query = "UPDATE npwd_calls SET is_accepted (?) WHERE trasmitter = ?";
+  await pool.query(query, [isAccepted, call.transmitter])
+}
+
+async function fetchCalls(phoneNumber: string): Promise<ICallUI[]> {
+  const query = "SELECT * FROM npwd_calls WHERE receiver = ? OR transmitter = ?"
+  const [result] = await pool.query(query, [phoneNumber, phoneNumber])
+  const calls = <ICallUI[]>result
+  return calls;
+}
+
 let calls: Map<string, ICall> = new Map()
+
 
 onNet(events.PHONE_INITIALIZE_CALL, async (phoneNumber: string) => {
   const _source = (global as any).source;
@@ -44,11 +65,12 @@ onNet(events.PHONE_INITIALIZE_CALL, async (phoneNumber: string) => {
   calls.set(transmitterNumber, {
     transmitter: transmitterNumber, 
     transmitterSource: _source,
+    transmitterIdentifier: xTransmitter.getIdentifier(),
     receiver: receiverNumber,
-    receiverSource: xReceiver.source
-  })
+    receiverSource: xReceiver.source,
+    receiverIdentifier: receiverIdentifier
+  }) 
   
-
   // events
   // client that is calling
   emitNet(events.PHONE_START_CALL, _source, transmitterNumber, receiverNumber, true);
@@ -56,9 +78,14 @@ onNet(events.PHONE_INITIALIZE_CALL, async (phoneNumber: string) => {
   // client that is being called
   emitNet(events.PHONE_START_CALL, xReceiver.source, transmitterNumber, receiverNumber, false);
 
+  const currentCall = calls.get(transmitterNumber)
+
+  await saveCall(currentCall, false)
+
 })
 
-onNet(events.PHONE_ACCEPT_CALL, (transmitterNumber: string) => {
+
+onNet(events.PHONE_ACCEPT_CALL, async (transmitterNumber: string) => {
   try {
     const pSource = (global as any).source
 
@@ -71,10 +98,13 @@ onNet(events.PHONE_ACCEPT_CALL, (transmitterNumber: string) => {
     // player who is calling
     emitNet(events.PHONE_CALL_WAS_ACCEPTED, currentCall.transmitterSource, channelId, currentCall, true)
 
+    await updateCall(currentCall, true);
+
   } catch (error) {
     console.log(error, error.message)
   }
 })
+
 
 onNet(events.PHONE_CALL_REJECTED, (transmitterNumber: string) => {
   try {
@@ -92,7 +122,8 @@ onNet(events.PHONE_CALL_REJECTED, (transmitterNumber: string) => {
   }
 })
 
-onNet(events.PHONE_END_CALL, (transmitterNumber: string) => {
+
+onNet(events.PHONE_END_CALL, async (transmitterNumber: string) => {
   try {
     const pSource = (global as any).source
     const currentCall = calls.get(transmitterNumber)
@@ -101,6 +132,8 @@ onNet(events.PHONE_END_CALL, (transmitterNumber: string) => {
     emitNet(events.PHONE_CALL_WAS_ENDED, currentCall.receiverSource)
     // player who is calling
     emitNet(events.PHONE_CALL_WAS_ENDED, currentCall.transmitterSource)
+
+    await updateCall(currentCall, true)
     
     calls.delete(transmitterNumber)
   
@@ -108,4 +141,16 @@ onNet(events.PHONE_END_CALL, (transmitterNumber: string) => {
   } catch (error) {
     console.log(error, error.message)
   }
+})
+
+
+onNet(events.PHONE_CALL_FETCH_CALLS, async () => {
+  const _source = (global as any).source;
+  const identifier = await useIdentifier()
+  const phoneNumber = await usePhoneNumber(identifier)
+
+  const calls = await fetchCalls(phoneNumber)
+
+  
+  emitNet(events.PHONE_CALL_SEND_HISTORY, _source, calls)
 })
