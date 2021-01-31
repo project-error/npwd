5563125d2cff37ac760ebbb47b273a5caa03dff7
Christopher <onlygamernerd@gmail.com>
2021-01-31 16:38:14 +0100

    fix(Notification): resolved ThemeProvider issue

--- phone/src/ui/components/Notification.tsx
+++ phone/src/ui/components/Notification.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Snackbar from '@material-ui/core/Snackbar';
 import Fade from '@material-ui/core/Fade';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { usePhone } from '../../os/phone/hooks/usePhone';
+
+import { useRecoilState } from 'recoil';
+import { settingsState } from '../../apps/settings/hooks/useSettings';
+import themeConfig from '../../config/default.json';
 
 const useStyles = makeStyles({
   paper: {
@@ -20,27 +25,32 @@ function Notification({ children, handleClose, open }) {
   const classes = useStyles();
   const { config } = usePhone();
 
+  const [settings] = useRecoilState(settingsState);
+
   if (!config) return null;
 
   const { horizontal, vertical } = config.notificationPosition;
+  const currentTheme = () => createMuiTheme(themeConfig.themes[settings.theme]);
 
   return (
-    <Snackbar
-      className={classes.snackBar}
-      anchorOrigin={{ horizontal, vertical }}
-      ClickAwayListenerProps={{
-        onClickAway: () =>
-          setTimeout(() => {
-            handleClose();
-          }, 5000),
-      }}
-      onClose={handleClose}
-      open={open}
-      TransitionComponent={Fade}
-      autoHideDuration={6000}
-    >
-      <Paper className={classes.paper}>{children}</Paper>
-    </Snackbar>
+    <ThemeProvider theme={currentTheme()}>
+      <Snackbar
+        className={classes.snackBar}
+        anchorOrigin={{ horizontal, vertical }}
+        ClickAwayListenerProps={{
+          onClickAway: () =>
+            setTimeout(() => {
+              handleClose();
+            }, 5000),
+        }}
+        onClose={handleClose}
+        open={open}
+        TransitionComponent={Fade}
+        autoHideDuration={6000}
+      >
+        <Paper className={classes.paper}>{children}</Paper>
+      </Snackbar>
+    </ThemeProvider>
   );
 }
 
