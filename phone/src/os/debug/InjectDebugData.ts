ada27686981610ece59b6734f1a9cd9c978fc958
Taso <taso@projecterror.dev>
2021-02-06 14:52:48 -0500

    feat(phone/debug): Inject debug data for development

---
+++ phone/src/os/debug/InjectDebugData.ts
@@ -0,0 +1,48 @@
+interface DebugEvent {
+  app: string;
+  method: string;
+  data: any;
+}
+
+// TODO: This should be unit tested
+
+/**
+ * Will mock data as if it were coming from an NUI message event
+ * @param events {DebugEvent[]} An array of debug events
+ * @example
+ * InjectDebugData([
+ *  {
+ *   app: 'CAMERA',
+ *   method: 'setPhotos',
+ *   data: [
+ *    {
+ *      id: 1,
+ *      image: 'https://i.imgur.com/OO8wx6Z.jpg',
+ *    },
+ *    {
+ *      id: 2,
+ *      image: 'https://i.imgur.com/OO8wx6Z.jpg',
+ *    }
+ *   ]
+ *  }
+ * ])
+ **/
+const InjectDebugData = (events: DebugEvent[]) => {
+  if (process.env.NODE_ENV === 'development') {
+    for (const event of events) {
+      setTimeout(() => {
+        window.dispatchEvent(
+          new MessageEvent('message', {
+            data: {
+              app: event.app,
+              method: event.method,
+              data: event.data,
+            },
+          })
+        );
+      }, 1000);
+    }
+  }
+};
+
+export default InjectDebugData;
