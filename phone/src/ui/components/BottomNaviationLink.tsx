4f1f667c4978f0f08a5aa1013fb189f5b6c96ef5
J Francisco Rader <franciscorader@gmail.com>
2021-02-07 17:36:28 -0500

    Delete unused component

--- phone/src/ui/components/BottomNaviationLink.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { forwardRef } from 'react';
-import { Link } from 'react-router-dom';
-
-export const BottomNavigationLink = (props) =>
-  forwardRef((linkProps, ref) => <Link ref={ref} {...linkProps} {...props} />);
