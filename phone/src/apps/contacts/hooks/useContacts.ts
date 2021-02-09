dadc26273477adb34173442d6850c6330149efef
Taso <Taso@projecterror.dev>
2021-02-08 20:28:01 -0500

    Added getContact function to useContacts hook

--- phone/src/apps/contacts/hooks/useContacts.ts
+++ phone/src/apps/contacts/hooks/useContacts.ts
@@ -7,13 +7,25 @@ import { useCallback } from 'react';
 interface IUseContacts {
   contacts: Contact[];
   getDisplayByNumber: (number: string) => string;
+  getContact: (id: number) => Contact;
 }
 
 export const useContacts = (): IUseContacts => {
   const contacts = useRecoilValue(contactsState.contacts);
-  const getDisplayByNumber = useCallback((number: string) => {
-    const found = contacts.find((contact) => contact.number === number) 
-    return found ? found.display : number
-  }, [contacts])
-  return { contacts, getDisplayByNumber };
+  const getDisplayByNumber = useCallback(
+    (number: string) => {
+      const found = contacts.find((contact) => contact.number === number);
+      return found ? found.display : number;
+    },
+    [contacts]
+  );
+  const getContact = useCallback(
+    (id: number): Contact | null => {
+      for (const contact of contacts) {
+        if (contact.id === id) return contact;
+      }
+    },
+    [contacts]
+  );
+  return { contacts, getDisplayByNumber, getContact };
 };
