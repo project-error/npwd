8ec2a4769d5f55fa23a129a53eb46ee8e22b78f1
Christopher <onlygamernerd@gmail.com>
2021-02-11 01:00:40 +0100

    feat(useSnackbar): strict types

--- phone/src/ui/hooks/useSnackbar.ts
+++ phone/src/ui/hooks/useSnackbar.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { SnackbarContext } from '../components/SnackbarProvider';
 
 interface ISnackBar {
-  addAlert: (message: string, type: string) => void;
+  addAlert: (message: string, type: 'success' | 'error') => void;
 }
 
 export const useSnackbar = (): ISnackBar => useContext(SnackbarContext);
