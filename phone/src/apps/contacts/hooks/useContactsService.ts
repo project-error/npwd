9846a4e1ae8202c17aa739e8fb305c8ae6af8301
Christopher <onlygamernerd@gmail.com>
2021-02-18 23:20:24 +0100

    feat(Alerts): alerts for contacts

--- phone/src/apps/contacts/hooks/useContactsService.ts
+++ phone/src/apps/contacts/hooks/useContactsService.ts
@@ -2,12 +2,12 @@ import { useNuiEvent } from '../../../os/nui-events/hooks/useNuiEvent';
 import { useSetRecoilState } from 'recoil';
 import { contactsState } from './state';
 import { useContacts } from './useContacts';
-import { useContactAlert } from './useAlert';
+import { useSnackbar } from '../../../ui/hooks/useSnackbar';
 
 export const useContactsService = () => {
   const setContacts = useSetRecoilState(contactsState.contacts);
-  const setAlert = useSetRecoilState(contactsState.contactAlert);
+  const { addAlert } = useSnackbar()
   useNuiEvent('CONTACTS', 'setContacts', setContacts);
-  useNuiEvent('CONTACTS', 'setAlert', setAlert);
-  return { useContacts, useContactAlert };
+  useNuiEvent('CONTACTS', 'setAlert', addAlert);
+  return useContacts();
 };
