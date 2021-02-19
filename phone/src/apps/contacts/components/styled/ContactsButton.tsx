ff177d65d1817293363e31f97de68e2ff9028f7f
J Francisco Rader <franciscorader@gmail.com>
2021-02-19 11:03:10 -0500

    Remove unused files

--- phone/src/apps/contacts/components/styled/ContactsButton.tsx
+++
@@ -1,7 +0,0 @@
-import { Button, withStyles } from '@material-ui/core';
-import { ButtonStyles } from '../../../../ui/helpers/customStyles';
-import { CONTACTS_APP_BG_COLOR, CONTACTS_APP_COLOR } from '../../constants';
-
-export const ContactsButton = withStyles(
-  ButtonStyles(CONTACTS_APP_COLOR, CONTACTS_APP_BG_COLOR)
-)(Button);
--- phone/src/apps/contacts/components/styled/ContactsTextField.tsx
+++
@@ -1,7 +0,0 @@
-import { TextField, withStyles } from '@material-ui/core';
-import { TextFieldStyles } from '../../../../ui/helpers/customStyles';
-import { CONTACTS_APP_BG_COLOR } from '../../constants';
-
-export const ContactsTextField = withStyles(
-  TextFieldStyles(CONTACTS_APP_BG_COLOR)
-)(TextField);
