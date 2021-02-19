5244ff3a57f2192780b1879aa6b0ccd791a126e5
Christopher <onlygamernerd@gmail.com>
2021-02-19 16:19:55 +0100

    feat(Marketplace): minor alerts

--- phone/src/apps/sellout/hooks/useSelloutService.ts
+++ phone/src/apps/sellout/hooks/useSelloutService.ts
@@ -2,9 +2,13 @@ import { useNuiEvent } from '../../../os/nui-events/hooks/useNuiEvent';
 import { selloutState } from './state';
 import { useListing } from './useListing';
 import { useSetRecoilState } from 'recoil';
+import { useSnackbar } from '../../../ui/hooks/useSnackbar';
 
 export const useSelloutService = () => {
   const setSellout = useSetRecoilState(selloutState.listing);
+  const { addAlert } = useSnackbar();
+
+  useNuiEvent('SELLOUT', 'setAlert', addAlert);
   useNuiEvent('SELLOUT', 'setListings', setSellout);
   return useListing();
 };
--- resources/client/cl_sellout.ts
+++ resources/client/cl_sellout.ts
@@ -12,11 +12,21 @@ onNet(events.SELLOUT_SEND_LISTING, (listing: any) => {
 
 RegisterNuiCallbackType(events.SELLOUT_ADD_LISTING);
 on(`__cfx_nui:${events.SELLOUT_ADD_LISTING}`, (data: any) => {
-  const listing = data;
-  emitNet(events.SELLOUT_ADD_LISTING, listing);
+  emitNet(events.SELLOUT_ADD_LISTING, data);
 
   setTimeout(() => {
     emitNet(events.SELLOUT_FETCH_LISTING);
     console.log('Fetched');
   }, 500);
 });
+
+
+onNet(events.SELLOUT_ACTION_RESULT, (alert: { message: string, type: string }) => {
+  SendNuiMessage(
+    JSON.stringify({
+      app: "SELLOUT",
+      method: "setAlert",
+      data: alert
+    })
+  )
+})
