7cc5ccb06aec57b500ec9cdd2ecbde398901f5b9
J Francisco Rader <franciscorader@gmail.com>
2021-02-20 19:09:50 -0500

    Improve createAppThemeProvider

--- phone/src/os/apps/utils/createAppThemeProvider.tsx
+++ phone/src/os/apps/utils/createAppThemeProvider.tsx
@@ -1,19 +1,16 @@
 import React, { useMemo } from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { deepMergeObjects } from './deepMergeObjects';
-import { useSettings } from '../../../apps/settings/hooks/useSettings';
-import config from './../../../config/default.json';
+import { usePhoneTheme } from '../../phone/hooks/usePhoneTheme';
 
 export function createAppThemeProvider(theme: {} = {}) {
   const AppThemeProvider = ({ children }: { children: React.ReactNode }) => {
-    const [settings] = useSettings();
+    const phoneTheme = usePhoneTheme();
     return (
       <ThemeProvider
         theme={useMemo(() => {
-          return createMuiTheme(
-            deepMergeObjects(config.themes[settings.theme], theme)
-          );
-        }, [settings.theme])}
+          return createMuiTheme(deepMergeObjects(phoneTheme, theme));
+        }, [phoneTheme])}
       >
         {children}
       </ThemeProvider>
