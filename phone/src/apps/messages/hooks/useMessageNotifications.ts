fc7548dadf072b8e5c71b151f6cd50c9dd7ada1b
Christopher <onlygamernerd@gmail.com>
2021-02-27 18:46:05 +0100

    last reformat! This pr will be squashed

--- phone/src/apps/messages/hooks/useMessageNotifications.ts
+++ phone/src/apps/messages/hooks/useMessageNotifications.ts
@@ -1,39 +1,44 @@
-import {useCallback} from "react";
-import {useTranslation} from 'react-i18next';
-import {useHistory} from 'react-router-dom';
-import {useRecoilState} from 'recoil';
-import {useApp} from '../../../os/apps/hooks/useApps';
-import {useNotifications} from "../../../os/notifications/hooks/useNotifications";
-import {messageState} from "./state";
+import { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { useApp } from '../../../os/apps/hooks/useApps';
+import { useNotifications } from '../../../os/notifications/hooks/useNotifications';
+import { messageState } from './state';
 
 export const useMessageNotifications = () => {
-	const {t} = useTranslation();
-	const history = useHistory();
-	const {addNotificationAlert} = useNotifications();
-	const {icon, notificationIcon} = useApp('MESSAGES');
-	const [unreadCount, setUnreadCount] = useRecoilState(messageState.unreadMessagesCount);
+  const { t } = useTranslation();
+  const history = useHistory();
+  const { addNotificationAlert } = useNotifications();
+  const { icon, notificationIcon } = useApp('MESSAGES');
+  const [unreadCount, setUnreadCount] = useRecoilState(
+    messageState.unreadMessagesCount
+  );
 
+  const setNotification = useCallback(
+    ({ number, message }) => {
+      setUnreadCount((curr: number) => {
+        const unread = curr + 1;
+        addNotificationAlert(
+          {
+            app: 'MESSAGES',
+            title: t('APPS_MESSAGES_NEW_BROADCAST', { number }),
+            onClick: () => history.push('/messages'),
+            content: message,
+            icon,
+            notificationIcon,
+          },
+          true,
+          {
+            title: t('APPS_MESSAGES_UNREAD_MESSAGES', { count: unread }),
+            content: null,
+          }
+        );
+        return unread;
+      });
+    },
+    [setUnreadCount, addNotificationAlert, t, icon, notificationIcon, history]
+  );
 
-	const setNotification = useCallback(({number, message}) => {
-		setUnreadCount((curr: number) => {
-			const unread = curr + 1;
-			addNotificationAlert({
-					app: 'MESSAGES',
-					title: t('APPS_MESSAGES_NEW_BROADCAST', {number}),
-					onClick: () => history.push('/messages'),
-					content: message,
-					icon,
-					notificationIcon
-				},
-				true,
-				{
-					title: t('APPS_MESSAGES_UNREAD_MESSAGES', {count: unread}),
-					content: null
-				}
-			);
-			return unread;
-		})
-	}, [setUnreadCount, addNotificationAlert, t, icon, notificationIcon, history])
-
-	return {setNotification, setUnreadCount, unreadCount};
-}
+  return { setNotification, setUnreadCount, unreadCount };
+};
