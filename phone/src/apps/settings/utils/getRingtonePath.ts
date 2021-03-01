178d0314a985b567131f111b8448e81d8de2d6ab
J Francisco Rader <franciscorader@gmail.com>
2021-03-01 14:06:03 -0500

    Fix ringtone paths

--- phone/src/apps/settings/utils/getRingtonePath.ts
+++ phone/src/apps/settings/utils/getRingtonePath.ts
@@ -1,5 +1,5 @@
 export const getRingtonePath = (name?: string) =>
-  `/media/ringtones/${name || 'pixel'}.ogg`;
+  `./media/ringtones/${name || 'pixel'}.ogg`;
 
 export const getNotificationPath = (name?: string) =>
-  `/media/notifications/${name || 'online'}.ogg`;
+  `./media/notifications/${name || 'online'}.ogg`;
