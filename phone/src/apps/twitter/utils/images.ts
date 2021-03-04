41b50d64298bfe0aa767aaae20f4e46b30332b7d
Taso <Taso@projecterror.dev>
2021-03-03 22:16:43 -0500

    tweak(phone/utils): Moved isImageValid util to common dir

--- phone/src/apps/twitter/utils/images.ts
+++ phone/src/apps/twitter/utils/images.ts
@@ -4,20 +4,3 @@
 // within specification). To get around this we have our own delimiter
 // here we'll use to join and split these strings
 export const IMAGE_DELIMITER = '||!||';
-
-/**
- * Returns a Promise that validates that the URL successfully
- * loads in the browser. If the URL doesn't load the promise is
- * rejected.
- * @param {string} link - a URL we are trying to validate
- * @returns {Promise<string>} link - Will resolve the promise with the link string
- * if valid. Will reject if failed.
- */
-export function isImageValid(link: string): Promise<string> {
-  return new Promise((res, rej) => {
-    const image = new Image();
-    image.src = link;
-    image.onerror = () => rej(`Invalid image, ${link}`);
-    image.onload = () => res(link);
-  });
-}
