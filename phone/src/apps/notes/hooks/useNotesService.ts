5c9c75c36c5e85e6efa125006f3db31d04a5ef34
Taso <taso@projecterror.dev>
2021-03-04 22:40:17 -0500

    tweak(phone/snackbar-provider): Altered API to expect a string for message, not a key (#135)
    Change SnackbarProviderAlert message property to allow for any string rather than a explicit translation key

--- phone/src/apps/notes/hooks/useNotesService.ts
+++ phone/src/apps/notes/hooks/useNotesService.ts
@@ -1,12 +1,21 @@
 import { useSetRecoilState } from 'recoil';
 import { useNuiEvent } from '../../../os/nui-events/hooks/useNuiEvent';
 import { noteStates } from './state';
-import { useSnackbar } from '../../../ui/hooks/useSnackbar';
+import { IAlert, useSnackbar } from '../../../ui/hooks/useSnackbar';
+import { useTranslation } from 'react-i18next';
 
 export const useNotesService = () => {
   const { addAlert } = useSnackbar();
   const setNotes = useSetRecoilState(noteStates.noteItems);
+  const { t } = useTranslation();
+
+  const handleAddAlert = ({ message, type }: IAlert) => {
+    addAlert({
+      message: t(`APPS_${message}`),
+      type,
+    });
+  };
 
   useNuiEvent('NOTES', 'setNotes', setNotes);
-  useNuiEvent('NOTES', 'setAlert', addAlert);
+  useNuiEvent('NOTES', 'setAlert', handleAddAlert);
 };
--- phone/src/apps/sellout/hooks/useSelloutService.ts
+++ phone/src/apps/sellout/hooks/useSelloutService.ts
@@ -2,13 +2,22 @@ import { useNuiEvent } from '../../../os/nui-events/hooks/useNuiEvent';
 import { selloutState } from './state';
 import { useListing } from './useListing';
 import { useSetRecoilState } from 'recoil';
-import { useSnackbar } from '../../../ui/hooks/useSnackbar';
+import { IAlert, useSnackbar } from '../../../ui/hooks/useSnackbar';
+import { useTranslation } from 'react-i18next';
 
 export const useSelloutService = () => {
   const setSellout = useSetRecoilState(selloutState.listing);
   const { addAlert } = useSnackbar();
+  const { t } = useTranslation();
 
-  useNuiEvent('SELLOUT', 'setAlert', addAlert);
+  const handleAddAlert = ({ message, type }: IAlert) => {
+    addAlert({
+      message: t(`APPS_${message}`),
+      type,
+    });
+  };
+
+  useNuiEvent('SELLOUT', 'setAlert', handleAddAlert);
   useNuiEvent('SELLOUT', 'setListings', setSellout);
   return useListing();
 };
