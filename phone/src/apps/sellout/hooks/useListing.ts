c21af2d297b3e1b5d550a4dfecf1f817a8796b35
Christopher <onlygamernerd@gmail.com>
2021-03-06 17:21:37 +0100

    tweak(marketplace/phone): simplified useListing hook'

--- phone/src/apps/sellout/hooks/useListing.ts
+++ phone/src/apps/sellout/hooks/useListing.ts
@@ -2,11 +2,6 @@ import { useRecoilValue } from 'recoil';
 import { MarketplaceListing } from '../../../common/typings/marketplace';
 import { selloutState } from './state';
 
-interface ListingHook {
-  listings: MarketplaceListing[];
-}
-
-export const useListing = (): ListingHook => {
-  const listings = useRecoilValue(selloutState.listing);
-  return { listings };
+export const useListing = () => {
+  return useRecoilValue<MarketplaceListing[]>(selloutState.listing);
 };
