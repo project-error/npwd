03520d459ce6d0b66af7246c8d9f48d960c99f7a
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-03-06 19:14:09 +0300

    Update i18n.ts

--- phone/src/i18n.ts
+++ phone/src/i18n.ts
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import en from './locale/en.json';
 import es from './locale/es.json';
 import fr from './locale/fr.json';
-import bh from './locale/bh.json';
+import bh from './locale/bs.json';
 import sv from './locale/sv.json';
 import tr from './locale/tr.json';
 import ptbr from './locale/ptbr.json';
