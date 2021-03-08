ebe75dba1f066f7d0e08ccbd1260395d0774818c
Christopher <onlygamernerd@gmail.com>
2021-03-08 19:56:29 +0100

    fix(phone/call): resolve showing transmitter number for transmitter

--- phone/src/os/call/components/CallModal.tsx
+++ phone/src/os/call/components/CallModal.tsx
@@ -6,16 +6,20 @@ import useStyles from './modal.styles';
 import CallTimer from './CallTimer';
 import { CallControls } from './CallControls';
 import { Box } from '@material-ui/core';
+import { useSimcard } from '../../simcard/hooks/useSimcard';
 
 export const CallModal = () => {
   const classes = useStyles();
   const { call } = useCall();
+  const { number: myNumber } = useSimcard();
 
   return (
     <AppWrapper>
       <AppContent>
         <Box pt={24}>
-          <h1 style={{ textAlign: 'center' }}>{call?.transmitter}</h1>
+          <h1 style={{ textAlign: 'center' }}>
+            {myNumber == call?.transmitter ? call?.receiver : call?.transmitter}
+          </h1>
           <CallTimer isAccepted={call?.accepted} />
         </Box>
         <div className={classes.actions}>
