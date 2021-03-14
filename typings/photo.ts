b6b46a6ee0bd6839a8d4c22bd8c8a1c46181480f
Taso <Taso@projecterror.dev>
2021-03-14 16:06:26 -0400

    chore(scripts/photos): Refactored photos to use event enums

--- typings/photo.ts
+++ typings/photo.ts
@@ -2,3 +2,13 @@ export interface IPhoto {
   image: string;
   id: number;
 }
+
+export enum PhotoEvents {
+  TAKE_PHOTO = 'phone:TakePhoto',
+  UPLOAD_PHOTO = 'phone:UploadPhoto',
+  UPLOAD_PHOTO_SUCCESS = 'phone:UploadPhotoSuccess',
+  FETCH_PHOTOS = 'phone:FetchPhotos',
+  SEND_PHOTOS = 'phone:SendPhotos',
+  DELETE_PHOTO = 'photo:deletePhoto',
+  DELETE_PHOTO_SUCCESS = 'phone:deletePhotoSuccess',
+}
