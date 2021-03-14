6717eed7f0978495e8433c447bb26096178e73ca
Taso <Taso@projecterror.dev>
2021-03-14 16:08:51 -0400

    chore(scripts/bank): Refactored bank to use event enums

--- typings/bank.ts
+++ typings/bank.ts
@@ -17,3 +17,14 @@ export interface ITransactions {
   type: string;
   amount: number;
 }
+
+export enum BankEvents {
+  ADD_TRANSFER = 'phone:addTransfer',
+  ADD_TRANSFER_SUCCESS = 'phone:addTransferSuccess',
+  FETCH_TRANSACTIONS = 'phone:fetchAllTransactions',
+  SEND_TRANSFERS = 'phone:sendTransfers',
+  TRANSACTION_ALERT = 'phone:bankTransactionAlert',
+  TRANSACTION_NOTIFICATION = 'phone:bankTransactionNotification',
+  SEND_CREDENTIALS = 'phone:sendBankCredentials',
+  GET_CREDENTIALS = 'phone:getBankCredentials',
+}
