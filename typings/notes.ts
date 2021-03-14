d0da2726ecb2a3ad925458b619ee5658cad8c453
Taso <Taso@projecterror.dev>
2021-03-14 16:04:12 -0400

    chore(scripts/notes): Refactored notes to use event enums

--- typings/notes.ts
+++ typings/notes.ts
@@ -20,3 +20,16 @@ export interface Note {
 export interface NoteId {
   id: number;
 }
+
+export enum NotesEvents {
+  ADD_NOTE = 'phone:addNote',
+  FETCH_ALL_NOTES = 'phone:fetchAllNotes',
+  SEND_NOTE = 'phone:sendNote',
+  SEND_NOTE_SUCCESS = 'phone:sendNoteSuccess',
+  DELETE_NOTE = 'phone:deleteNote',
+  DELETE_NOTE_SUCCESS = 'phone:deleteNoteSuccess',
+  UPDATE_NOTE = 'phone:updateNote',
+  UPDATE_NOTE_SUCCESS = 'phone:updateNoteSuccess',
+  UPDATE_NOTE_FAILURE = 'phone:updateNoteFailure',
+  ACTION_RESULT = 'phone:notesActionResult',
+}
