f3076a45fbe52d33f83f0ddf200415d9db477107
Darrian <darriangaylor@icloud.com>
2021-04-01 17:21:22 -0400

    RFR: Remove items in SQL

--- import.sql
+++ import.sql
@@ -1,8 +1,6 @@
 # comment this out if your server has a different table/location for phone_number
 # or if it already exists in the users table
 ALTER TABLE `users` ADD COLUMN `phone_number` VARCHAR(20) DEFAULT NULL;
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('phone', 'Phone', 1, 0, 1);
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('dphone', 'Destroyed Phone', 1, 0, 1);
 
 CREATE TABLE IF NOT EXISTS `npwd_phone_contacts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
