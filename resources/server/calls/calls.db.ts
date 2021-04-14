d93bdd20d8d30d0c35a72021044f89152e828e7f
Taso <taso@projecterror.dev>
2021-04-14 16:41:09 -0400

    tweak(server/calls): Tweaked structure and added enum constants

--- resources/server/calls/calls.db.ts
+++ resources/server/calls/calls.db.ts
@@ -1,24 +1,28 @@
 import { CallHistoryItem } from '../../../typings/call';
 import { pool } from '../db';
+import { FetchDefaultLimits } from '../utils/ServerConstants';
 
 export class CallsRepo {
-  async saveCall(call: CallHistoryItem) {
+  async saveCall(call: CallHistoryItem): Promise<void> {
     const query =
-      'INSERT INTO npwd_calls (identifier, transmitter, receiver, start) VALUES (?, ?, ?, ?)';
+      'INSERT INTO npwd_calls (identifier, transmitter, receiver, `start`) VALUES (?, ?, ?, ?)';
     await pool.query(query, [call.identifier, call.transmitter, call.receiver, call.start]);
   }
 
-  async updateCall(call: CallHistoryItem, isAccepted: boolean, end: number) {
+  async updateCall(call: CallHistoryItem, isAccepted: boolean, end: number): Promise<void> {
     const query = 'UPDATE npwd_calls SET is_accepted=?, end=? WHERE identifier = ?';
     await pool.query(query, [isAccepted, end, call.identifier]);
   }
 
-  async fetchCalls(phoneNumber: string, limit = 50): Promise<CallHistoryItem[] | CallHistoryItem> {
+  async fetchCalls(
+    phoneNumber: string,
+    limit = FetchDefaultLimits.CALLS_FETCH_LIMIT,
+  ): Promise<CallHistoryItem[] | CallHistoryItem> {
     const query =
       'SELECT * FROM npwd_calls WHERE receiver = ? OR transmitter = ? ORDER BY id DESC LIMIT ?';
     const [result] = await pool.query(query, [phoneNumber, phoneNumber, limit]);
 
-    return <CallHistoryItem[]>result;
+    return <CallHistoryItem[] | CallHistoryItem>result;
   }
 }
 
---
+++ resources/server/utils/ServerConstants.ts
@@ -0,0 +1,4 @@
+export enum FetchDefaultLimits {
+  NOTES_FETCH_LIMIT = 20,
+  CALLS_FETCH_LIMIT = 50,
+}
