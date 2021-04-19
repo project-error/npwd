DmACKGL/new-phone-who-dis

tweak(player): Altered PLAYER_IS_READY event to emit a bool
This will set the client var to true when NPWD loads a player and false when NPWD unloads a player.

resources/client/cl_main.ts
@@ -10,7 +10,6 @@ import { NotesEvents } from '../../typings/notes';
 import { BankEvents } from '../../typings/bank';
 import { PhotoEvents } from '../../typings/photo';
 import { CallEvents } from '../../typings/call';
-import { MatchEvents } from '../../typings/match';
 
 let isPhoneOpen = false;
 let isPhoneReady = false;
@@ -21,7 +20,6 @@ let isPhoneReady = false;
  *
  * * * * * * * * * * * * */
 function fetchOnInitialize() {
-  isPhoneReady = true;
   emitNet(ContactEvents.GET_CONTACTS);
   emitNet(MessageEvents.FETCH_MESSAGE_GROUPS);
   emitNet(TwitterEvents.GET_OR_CREATE_PROFILE);
@@ -110,8 +108,10 @@ async function Phone(): Promise<void> {
   }
 }
 
-// triggerd when the player is ready
-onNet(PhoneEvents.PLAYER_IS_READY, fetchOnInitialize);
+// Set the global depending on the response from event
+onNet(PhoneEvents.PLAYER_IS_READY, (ready: boolean) => {
+  isPhoneReady = ready;
+});
 
 AddEventHandler('onResourceStop', function (resource: string) {
   if (resource === GetCurrentResourceName()) {

project-error/npwd

resources/client/cl_main.ts
@@ -9,7 +9,6 @@ import { NotesEvents } from '../../typings/notes';
 import { BankEvents } from '../../typings/bank';
 import { PhotoEvents } from '../../typings/photo';
 import { CallEvents } from '../../typings/call';
-import { MatchEvents } from '../../typings/match';
 import { config } from './client';
 
 let isPhoneOpen = false;
@@ -109,12 +108,9 @@ async function Phone(): Promise<void> {
   }
 }
 
-// triggerd when the player is ready
-onNet(PhoneEvents.PLAYER_IS_READY, () => {
-  isPlayerReady = true;
-  if (isUiReady) {
-    fetchOnInitialize();
-  }
+// Set the global depending on the response from event
+onNet(PhoneEvents.PLAYER_IS_READY, (ready: boolean) => {
+  isPlayerReady = ready;
 });
 
 AddEventHandler('onResourceStop', function (resource: string) {