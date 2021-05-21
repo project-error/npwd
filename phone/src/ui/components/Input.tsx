89f39493fb833316c9c7b695d093321d5631c8c0
NuttyShrimp <jan.lecoutere@hotmail.be>
2021-05-21 02:22:18 +0200

    refactor(phone) renamed var & replaced copy paste code
    Instead of copying the code it now uses a custom hook

--- phone/src/ui/components/Input.tsx
+++ phone/src/ui/components/Input.tsx
@@ -4,50 +4,54 @@ import MUIInputBase from '@material-ui/core/InputBase';
 import { useNuiRequest } from 'fivem-nui-react-lib';
 import { PhoneEvents } from '../../../../typings/phone';
 
+const ToggleKeys = ()=>{
+	const Nui = useNuiRequest();
+	const toggle = React.useCallback((keepGameFocus:boolean)=>{
+		Nui.send(PhoneEvents.TOGGLE_KEYS, {
+			keepGameFocus,
+		});
+	}, [Nui])
+	return React.useMemo(()=>({toggle}), [toggle])
+}
+
 export const TextField = (props) => {
-  const Nui = useNuiRequest();
-
-  const handleFocus = (e) => {
-    Nui.send(PhoneEvents.TOGGLE_KEYS, {
-      bool: true,
-    });
-    if (props.onFocus) {
-      props.onFocus(e);
-    }
-  };
-
-  const handleUnFocus = (e) => {
-    Nui.send(PhoneEvents.TOGGLE_KEYS, {
-      bool: false,
-    });
-    if (props.onBlur) {
-      props.onBlur(e);
-    }
-  };
-
-  return <MUITextField {...props} onFocus={handleFocus} onBlur={handleUnFocus} />;
+	const KeysHandler = ToggleKeys()
+  return(
+		<MUITextField
+			{...props}
+			onFocus={(e)=>{
+				KeysHandler.toggle(false)
+				if (props.onFocus) {
+					props.onFocus(e);
+				}
+			}}
+			onBlur={(e)=>{
+				KeysHandler.toggle(true)
+				if (props.onBlur) {
+					props.onBlur(e);
+				}
+			}}
+		/>
+	);
 };
 
 export const InputBase = (props) => {
-  const Nui = useNuiRequest();
-
-  const handleFocus = (e) => {
-    Nui.send(PhoneEvents.TOGGLE_KEYS, {
-      bool: true,
-    });
-    if (props.onFocus) {
-      props.onFocus(e);
-    }
-  };
-
-  const handleUnFocus = (e) => {
-    Nui.send(PhoneEvents.TOGGLE_KEYS, {
-      bool: false,
-    });
-    if (props.onBlur) {
-      props.onBlur(e);
-    }
-  };
-
-  return <MUIInputBase onFocus={handleFocus} onBlur={handleUnFocus} {...props} />;
+	const KeysHandler = ToggleKeys()
+  return (
+		<MUIInputBase
+	 		{...props}
+			onFocus={(e)=>{
+				KeysHandler.toggle(false)
+				if (props.onFocus) {
+					props.onFocus(e);
+				}
+			}}
+			onBlur={(e)=>{
+				KeysHandler.toggle(true)
+				if (props.onBlur) {
+					props.onBlur(e);
+				}
+			}}
+		/>
+	 )
 };
