b2459c8a8f3a487be67bf2326e7a1b0ad2db82be
Jan Lecoutere <jan.lecoutere@hotmail.be>
2021-05-21 13:10:01 +0200

    refactor(phone/input): removed useMemo from hook

--- phone/src/ui/components/Input.tsx
+++ phone/src/ui/components/Input.tsx
@@ -4,54 +4,56 @@ import MUIInputBase from '@material-ui/core/InputBase';
 import { useNuiRequest } from 'fivem-nui-react-lib';
 import { PhoneEvents } from '../../../../typings/phone';
 
-const ToggleKeys = ()=>{
-	const Nui = useNuiRequest();
-	const toggle = React.useCallback((keepGameFocus:boolean)=>{
-		Nui.send(PhoneEvents.TOGGLE_KEYS, {
-			keepGameFocus,
-		});
-	}, [Nui])
-	return React.useMemo(()=>({toggle}), [toggle])
-}
+const useToggleFocus = () => {
+  const Nui = useNuiRequest();
+  return React.useCallback(
+    (keepGameFocus: boolean) => {
+      Nui.send(PhoneEvents.TOGGLE_KEYS, {
+        keepGameFocus,
+      });
+    },
+    [Nui],
+  );
+};
 
 export const TextField = (props) => {
-	const KeysHandler = ToggleKeys()
-  return(
-		<MUITextField
-			{...props}
-			onFocus={(e)=>{
-				KeysHandler.toggle(false)
-				if (props.onFocus) {
-					props.onFocus(e);
-				}
-			}}
-			onBlur={(e)=>{
-				KeysHandler.toggle(true)
-				if (props.onBlur) {
-					props.onBlur(e);
-				}
-			}}
-		/>
-	);
+  const toggleKeys = useToggleFocus();
+  return (
+    <MUITextField
+      {...props}
+      onFocus={(e) => {
+        toggleKeys(false);
+        if (props.onFocus) {
+          props.onFocus(e);
+        }
+      }}
+      onBlur={(e) => {
+        toggleKeys(true);
+        if (props.onBlur) {
+          props.onBlur(e);
+        }
+      }}
+    />
+  );
 };
 
 export const InputBase = (props) => {
-	const KeysHandler = ToggleKeys()
+  const toggleKeys = useToggleFocus();
   return (
-		<MUIInputBase
-	 		{...props}
-			onFocus={(e)=>{
-				KeysHandler.toggle(false)
-				if (props.onFocus) {
-					props.onFocus(e);
-				}
-			}}
-			onBlur={(e)=>{
-				KeysHandler.toggle(true)
-				if (props.onBlur) {
-					props.onBlur(e);
-				}
-			}}
-		/>
-	 )
+    <MUIInputBase
+      {...props}
+      onFocus={(e) => {
+        toggleKeys(false);
+        if (props.onFocus) {
+          props.onFocus(e);
+        }
+      }}
+      onBlur={(e) => {
+        toggleKeys(true);
+        if (props.onBlur) {
+          props.onBlur(e);
+        }
+      }}
+    />
+  );
 };
