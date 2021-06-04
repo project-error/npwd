b257f5bc734e0440c24ce699797421742a99edce
Taso <taso@projecterror.dev>
2021-06-04 11:55:03 -0400

    tweak(server/onNetPromise): Tweak to be less opinionated and simpler

--- resources/server/utils/PromiseNetEvents/promise.types.ts
+++ resources/server/utils/PromiseNetEvents/promise.types.ts
@@ -1,30 +1,8 @@
-import { FxServerRespError } from '../../../../typings/phone';
-import { PromiseResponse } from './PromiseResponse';
-
-export type BasicPrimitives = string | number | boolean;
-
-export interface JSONLike {
-  [key: string]: BasicPrimitives;
-}
-
-export type PromiseRespStatus = 'error' | 'success' | 'warning' | 'in-progress';
-
-export interface RawDataResp {
-  status: PromiseRespStatus;
-  error?: FxServerRespError;
-  body: JSONLike;
-}
-
 export interface PromiseRequest<T> {
-  body: PromiseRequestData<T>;
+  data: T;
   source: number;
 }
 
-export interface PromiseRequestData<T> {
-  data: T;
-}
+export type PromiseEventReturnFunc = (returnData: unknown) => void;
 
-export type CBSignature<ReqStruct> = (
-  request: PromiseRequest<ReqStruct>,
-  response: PromiseResponse,
-) => Promise<void> | void;
+export type CBSignature<T> = (reqObj: PromiseRequest<T>, resp: PromiseEventReturnFunc) => void;
