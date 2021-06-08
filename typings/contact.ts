51d8aa632267b88160cba19cced3d8d595bc8165
Taso <taso@projecterror.dev>
2021-06-08 15:36:56 -0400

    types(contacts): Cleanup unused types

--- typings/contact.ts
+++ typings/contact.ts
@@ -6,11 +6,6 @@ export type AlertCategory =
   | 'CONTACT_DELETE_SUCCESS'
   | 'CONTACT_DELETE_FAILED';
 
-export interface IContactAlert {
-  alert: AlertCategory;
-  setAlert: (type: string) => void;
-}
-
 export interface PreDBContact {
   display: string;
   number: string;
@@ -26,15 +21,8 @@ export interface ContactDeleteDTO {
 }
 
 export enum ContactEvents {
-  SEND_CONTACTS = 'npwd:sendContacts',
   GET_CONTACTS = 'npwd:getContacts',
   ADD_CONTACT = 'npwd:addContacts',
-  ADD_CONTACT_SUCCESS = 'contacts:addSuccess',
   UPDATE_CONTACT = 'npwd:updateContact',
   DELETE_CONTACT = 'npwd:deleteContact',
-  DELETE_CONTACT_SUCCESS = 'npwd:deleteSuccess',
-  UPDATE_CONTACT_SUCCESS = 'npwd:updateContactSuccess',
-  ACTION_RESULT = 'npwd:contactActionResult',
-  SEND_ALERT = 'npwd:contactSetAlert',
-  MAIN_CLIENT_LISTER = 'npwd:contactMainListener',
 }
