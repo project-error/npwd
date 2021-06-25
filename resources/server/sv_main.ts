4ef89f679f4ec8bda652d7fd27d8e24abd3b458b
Christopher <onlygamernerd@gmail.com>
2021-06-25 21:15:45 +0200

    fix(resources/server): changed credentials return type

--- resources/server/sv_main.ts
+++ resources/server/sv_main.ts
@@ -5,14 +5,10 @@ import PlayerService from './players/player.service';
 import { getSource } from './utils/miscUtils';
 import { config } from './server';
 
-interface Credentials {
-  phone_number: string;
-}
-
 async function getCredentials(identifier: string): Promise<string> {
   const query = `SELECT phone_number FROM ${config.database.playerTable} WHERE ${config.database.identifierColumn} = ?`;
   const [result] = await pool.query(query, [identifier]);
-  const number = <Credentials[]>result;
+  const number = <any[]>result;
   if (number.length === 0) return '###-####';
   return number[0].phone_number;
 }
