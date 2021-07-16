d085133a366cb14bbf013725953e187421af7b20
Christopher <onlygamernerd@gmail.com>
2021-07-16 15:57:11 +0200

    chore(phone): minor changes

---
+++ phone/src/apps/dialer/utils/constants.ts
@@ -0,0 +1,20 @@
+export const MockHistoryData = [
+  {
+    id: 1,
+    identifier: 'ca839e8c-ef9d-40bd-b6a3-d531ee58149f',
+    transmitter: '704-1549',
+    receiver: '704-1549',
+    is_accepted: 0,
+    start: '1615939174',
+    end: null,
+  },
+  {
+    id: 2,
+    identifier: 'b8080a50-ad30-4ee9-84cb-00688e2bf167',
+    transmitter: '704-1549',
+    receiver: '704-1549',
+    is_accepted: 0,
+    start: '1615939850',
+    end: null,
+  },
+];
