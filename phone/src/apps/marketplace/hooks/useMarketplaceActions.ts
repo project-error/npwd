85fd85ad6a56dea28f108e3590f4d66a9ab6d55c
Taso <taso@projecterror.dev>
2021-07-23 08:51:21 -0400

    fix(phone/marketplace): fix state dispatch when selector is loading
    Should fix edge cases where broadcast events are received and processed before the original fetch promise resolves

--- phone/src/apps/marketplace/hooks/useMarketplaceActions.ts
+++ phone/src/apps/marketplace/hooks/useMarketplaceActions.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
-import { useSetListings } from './state';
+import { listingState, useSetListings } from './state';
 import { MarketplaceListing } from '../../../../../typings/marketplace';
+import { useRecoilValueLoadable } from 'recoil';
 
 interface MarketplaceActionValues {
   deleteListing: (id: number) => void;
@@ -8,20 +9,29 @@ interface MarketplaceActionValues {
 }
 
 export const useMarketplaceActions = (): MarketplaceActionValues => {
+  const { state: valueLoading, contents } = useRecoilValueLoadable(listingState);
   const setListings = useSetListings();
 
   const deleteListing = useCallback(
     (id: number) => {
+      // Make sure our atom is actually loaded before we attempt a dispatch
+      if (valueLoading !== 'hasValue') return;
+      // Tts possible for the init fetch to fail and empty array is passed. In that case
+      // block dispatch
+      if (!contents.length) return;
+
       setListings((curListings) => [...curListings].filter((listing) => listing.id !== id));
     },
-    [setListings],
+    [setListings, valueLoading, contents],
   );
 
   const addListing = useCallback(
     (listing: MarketplaceListing) => {
+      // Make sure our atom is actually loaded before we attempt a dispatch
+      if (valueLoading !== 'hasValue') return;
       setListings((curListings) => [...curListings, listing]);
     },
-    [setListings],
+    [setListings, valueLoading, contents],
   );
 
   return { deleteListing, addListing };
