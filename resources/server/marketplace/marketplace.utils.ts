60b9e35ae8727b7e56ef86aa6b50c4684ab33f78
Christopher <onlygamernerd@gmail.com>
2021-07-29 20:25:58 +0200

    fix(marketplace): should probably change the name of the loger too

--- resources/server/marketplace/marketplace.utils.ts
+++ resources/server/marketplace/marketplace.utils.ts
@@ -1,3 +1,3 @@
 import { mainLogger } from '../sv_logger';
 
-export const marketplaceLogger = mainLogger.child({ module: 'sellout' });
+export const marketplaceLogger = mainLogger.child({ module: 'marketplace' });
