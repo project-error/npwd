8461c1acd2fd0ce6eccb633c87de810d598d3858
Taso <taso@projecterror.dev>
2021-08-05 20:31:00 -0400

    tweak(phone): tweaked env handling to expose package.json version for sentry

--- phone/src/index.tsx
+++ phone/src/index.tsx
@@ -20,7 +20,7 @@ if (PhoneConfig.SentryErrorMetrics && process.env.NODE_ENV !== 'development') {
   Sentry.init({
     dsn: 'https://0c8321c22b794dc7a648a571cc8c3c34@sentry.projecterror.dev/2',
     autoSessionTracking: true,
-    release: process.env.REACT_APP_SENTRY_RELEASE || '0.0.0',
+    release: process.env.REACT_APP_VERSION,
     integrations: [new Integrations.BrowserTracing()],
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
