4f3fdd941e4d3fdcca9da45ec9fafe04632cda4a
Christopher <onlygamernerd@gmail.com>
2021-08-29 10:30:53 +0200

    fix(example): update state example files

--- phone/src/apps/example/components/ExampleApp.tsx
+++ phone/src/apps/example/components/ExampleApp.tsx
@@ -3,17 +3,17 @@ import React, { useState } from 'react';
 import { AppWrapper } from '../../../ui/components';
 import { AppContent } from '../../../ui/components/AppContent';
 import { useApp } from '../../../os/apps/hooks/useApps';
-import { useExample } from '../hooks/useExample';
 import { ExampleThemeProvider } from '../providers/ExampleThemeProvider';
 import { AppTitle } from '../../../ui/components/AppTitle';
 import PlayIcon from '@material-ui/icons/PlayArrow';
 import StopIcon from '@material-ui/icons/Stop';
 import { Button, IconButton } from '@material-ui/core';
 import useSound from '../../../os/sound/hooks/useSound';
+import { useExampleStringValue } from '../hooks/state';
 
 export const ExampleApp = () => {
   // calling the example hook, and we assign the value to a variable
-  const exampleString = useExample();
+  const exampleString = useExampleStringValue();
   const example = useApp('EXAMPLE');
   const [soundName, setSound] = useState('pixel.ogg');
   const { play, playing, stop } = useSound('media/ringtones/' + soundName);
