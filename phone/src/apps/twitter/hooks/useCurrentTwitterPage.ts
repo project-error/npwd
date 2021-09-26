cf549e1c157a1b784e5e73553db885bfe1ca6886
Léderer András <andrew@file.glass>
2021-09-26 18:42:18 +0200

    refactor(phone/twitter/useCurrentTwitterPage): one liners

--- phone/src/apps/twitter/hooks/useCurrentTwitterPage.ts
+++ phone/src/apps/twitter/hooks/useCurrentTwitterPage.ts
@@ -3,15 +3,11 @@ import { twitterState } from './state';
 
 export const useCurrentTwitterPage = () => {
   const [currId, setCurr] = useRecoilState(twitterState.tweetPageId);
-  const increase = (val = 1) => {
-    setCurr(currId + val);
-  };
-  const decrease = (val = 1) => {
-    setCurr(currId - val);
-  };
-  const set = (val = 0) => {
-    setCurr(val);
-  };
+  const increase = (val = 1) => setCurr(currId + val);
+
+  const decrease = (val = 1) => setCurr(currId - val);
+
+  const set = (val = 0) => setCurr(val);
 
   return { increase, decrease, set, pageId: currId };
 };
