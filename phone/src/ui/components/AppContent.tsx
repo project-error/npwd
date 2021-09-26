60a560424ec5eaf97315558cf09780168bd11472
Taso <taso@projecterror.dev>
2021-09-25 21:34:38 -0400

    fix(phone/AppContent): Dynamically set AppContent height
    This should fix any issues with overflow and the parent containers background color

--- phone/src/ui/components/AppContent.tsx
+++ phone/src/ui/components/AppContent.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Box, Backdrop, Paper, BoxProps } from '@mui/material';
+import { Backdrop, Paper, BoxProps } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { AppContentTypes } from '../interface/InterfaceUI';
 
 const useStyles = makeStyles(() => ({
   wrapper: {
     flex: 1,
-  },
-  box: {
-    width: '100%',
-    height: '100%', // allow application to fill entireity of space
+    flexDirection: 'column',
+    display: 'flex',
   },
   backdrop: {
     position: 'absolute',
@@ -17,7 +15,7 @@ const useStyles = makeStyles(() => ({
   },
   paper: {
     width: '100%',
-    height: '100%', // allow application to fill entireity of space
+    flex: '1 1 auto', // allow application to fill entireity of space
   },
 }));
 
@@ -37,16 +35,15 @@ export const AppContent: React.FC<AppContentTypes & BoxProps> = ({
       style={backdrop ? { overflow: 'hidden' } : { overflow: 'auto' }}
     >
       <Backdrop className={classes.backdrop} open={backdrop || false} onClick={onClickBackdrop} />
-      <Box flexGrow={1} className={classes.box} {...props}>
-        <Paper
-          square
-          elevation={0}
-          className={`${classes.paper} ${props.className}`}
-          style={paperStyle}
-        >
-          {children}
-        </Paper>
-      </Box>
+      <Paper
+        sx={{ flexGrow: 1 }}
+        square
+        elevation={0}
+        className={`${classes.paper} ${props.className}`}
+        style={paperStyle}
+      >
+        {children}
+      </Paper>
     </Paper>
   );
 };
