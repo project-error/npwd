f544e16a752c108f9495ea97c42e4bfe6ecaa976
Taso <taso@projecterror.dev>
2021-11-28 15:26:16 -0500

    feat(phone/debug): add setPhoneVisible method to npwdDebug

--- phone/src/os/debug/AttachWindowDebug.ts
+++ phone/src/os/debug/AttachWindowDebug.ts
@@ -1,3 +1,5 @@
+import { PhoneEvents } from '../../../../typings/phone';
+
 function dispatchEvent({ method, app, data = {} }: { method: string; app: string; data: unknown }) {
   setTimeout(() => {
     window.dispatchEvent(
@@ -17,6 +19,13 @@ const debugObj = {
     dispatchEvent({ method: 'notiTest', app: 'PHONE', data: {} });
   },
   mockNuiEvent: dispatchEvent,
+  setPhoneVisible: (bool: boolean) => {
+    dispatchEvent({
+      method: PhoneEvents.SET_VISIBILITY,
+      data: bool,
+      app: 'PHONE',
+    });
+  },
 };
 
 const attachWindowDebug = () => {
