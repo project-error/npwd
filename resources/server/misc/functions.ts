22c0d583466c824bda74fd790c8d1719be8fb9d2
Linden <65407488+thelindat@users.noreply.github.com>
2021-12-02 10:30:15 +1100

    refactor(server/functions): replace _rawExec
    Utilise fetch and exec from DbInterface.
    Resolved awkward typing with changes to db/wrapper.

--- resources/server/misc/functions.ts
+++ resources/server/misc/functions.ts
@@ -5,15 +5,10 @@ import { playerLogger } from '../players/player.utils';
 
 export async function findOrGeneratePhoneNumber(identifier: string): Promise<string> {
   const query = `SELECT ${config.database.phoneNumberColumn} FROM ${config.database.playerTable} WHERE ${config.database.identifierColumn} = ? LIMIT 1`;
-  const [res] = await DbInterface._rawExec(query, [identifier]);
+  const res = await DbInterface.fetch(query, [identifier]);
 
-  playerLogger.debug('Find user for number generation data >');
-  playerLogger.debug(res);
-
-  const castRes = res as Record<string, unknown>[];
-
-  if (castRes && castRes[0][config.database.phoneNumberColumn] !== null)
-    return castRes[0][config.database.phoneNumberColumn] as string;
+  if (res && res[0][config.database.phoneNumberColumn])
+    return res[0][config.database.phoneNumberColumn] as string;
 
   playerLogger.debug('Phone number was returned as null, generating new number');
   const gennedNumber = await generateUniquePhoneNumber();
@@ -22,7 +17,7 @@ export async function findOrGeneratePhoneNumber(identifier: string): Promise<str
 
   const updateQuery = `UPDATE ${config.database.playerTable} SET ${config.database.phoneNumberColumn} = ? WHERE ${config.database.identifierColumn} = ?`;
   // Update profile with new generated number
-  await DbInterface._rawExec(updateQuery, [gennedNumber, identifier]);
+  await DbInterface.exec(updateQuery, [gennedNumber, identifier]);
 
   return gennedNumber;
 }
