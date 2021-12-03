12bf2e682364ffc6c0070b7acc1cd4133447f2ac
Taso <taso@projecterror.dev>
2021-12-02 19:51:32 -0500

    tweak(phone/marketplace): lazily load marketplace listings

--- phone/src/apps/marketplace/hooks/useMarketplaceActions.ts
+++ phone/src/apps/marketplace/hooks/useMarketplaceActions.ts
@@ -1,7 +1,6 @@
-import { useCallback } from 'react';
 import { listingState, useSetListings } from './state';
 import { MarketplaceListing } from '@typings/marketplace';
-import { useRecoilValueLoadable } from 'recoil';
+import { useRecoilCallback } from 'recoil';
 
 interface MarketplaceActionValues {
   deleteListing: (id: number) => void;
@@ -9,29 +8,33 @@ interface MarketplaceActionValues {
 }
 
 export const useMarketplaceActions = (): MarketplaceActionValues => {
-  const { state: valueLoading, contents } = useRecoilValueLoadable(listingState);
   const setListings = useSetListings();
 
-  const deleteListing = useCallback(
-    (id: number) => {
-      // Make sure our atom is actually loaded before we attempt a dispatch
-      if (valueLoading !== 'hasValue') return;
-      // Tts possible for the init fetch to fail and empty array is passed. In that case
-      // block dispatch
-      if (!contents.length) return;
+  const deleteListing = useRecoilCallback(
+    ({ snapshot }) =>
+      (id: number) => {
+        const { state, contents } = snapshot.getLoadable(listingState);
+        // Make sure our atom is actually loaded before we attempt a dispatch
+        if (state !== 'hasValue') return;
+        // Tts possible for the init fetch to fail and empty array is passed. In that case
+        // block dispatch
+        if (!contents.length) return;
 
-      setListings((curListings) => [...curListings].filter((listing) => listing.id !== id));
-    },
-    [setListings, valueLoading, contents],
+        setListings((curListings) => [...curListings].filter((listing) => listing.id !== id));
+      },
+    [],
   );
 
-  const addListing = useCallback(
-    (listing: MarketplaceListing) => {
-      // Make sure our atom is actually loaded before we attempt a dispatch
-      if (valueLoading !== 'hasValue') return;
-      setListings((curListings) => [listing, ...curListings]);
-    },
-    [setListings, valueLoading],
+  const addListing = useRecoilCallback(
+    ({ snapshot }) =>
+      (listing: MarketplaceListing) => {
+        const { contents } = snapshot.getLoadable(listingState);
+        // Make sure our atom is actually loaded before we attempt a dispatch
+        if (contents !== 'hasValue') return;
+
+        setListings((curListings) => [listing, ...curListings]);
+      },
+    [setListings],
   );
 
   return { deleteListing, addListing };
