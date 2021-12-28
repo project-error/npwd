131c9508c4ccae3c2d3e42851fa5237c7fe94fa7
Taso <taso@projecterror.dev>
2021-12-28 03:39:35 -0500

    feat(local/de): configure new german locale

--- phone/src/i18n.ts
+++ phone/src/i18n.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locale/en.json';
 import es from './locale/es.json';
+import de from './locale/de.json';
 import fr from './locale/fr.json';
 import ba from './locale/ba.json';
 import sv from './locale/sv.json';
@@ -22,6 +23,7 @@ export const resources = {
   ba,
   no,
   sv,
+  de,
   tr,
   ptbr,
   pt,
