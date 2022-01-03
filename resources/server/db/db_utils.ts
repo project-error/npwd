93e1c1e2f4981ca1aae2378904d85475d910eb4c
Linden <65407488+thelindat@users.noreply.github.com>
2022-01-04 08:11:47 +1100

    refactor(db): change mysql_connection_string parsing (#464)
    * chore(db/test): connection string parser
    
    Simplify to match updated connection string parser, and check different strings.
    
    * refactor(db/utils): parseSemiColonFormat accepted values
    
    Replace string values before splitting, using regex to assign expected names.
    
    * refactor(db/pool): remove connection string parser
    
    Sends URI connection strings directly to node-mysql2, which handles parsing more consistently.

--- resources/server/db/db_utils.ts
+++ resources/server/db/db_utils.ts
@@ -1,8 +1,4 @@
 export const CONNECTION_STRING = 'mysql_connection_string';
-export const DEFAULT_PORT = 3306;
-export const DEFAULT_HOST = 'localhost';
-export const DEFAULT_USER = 'root';
-
 interface Map {
   [key: string]: string;
 }
@@ -12,59 +8,22 @@ interface Map {
  * @param connectionString - mysql_connection_string value
  */
 export function parseSemiColonFormat(connectionString: string): Map {
-  const parts = connectionString.split(';');
+  const parts = connectionString
+    .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
+    .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
+    .replace(/(?:pwd|pass)=/gi, 'password=')
+    .replace(/(?:db)=/gi, 'database=')
+    .split(';');
+
   if (parts.length === 1) {
     throw new Error(
       `Connection string ${connectionString} is in the incorrect format. Please follow the README.`,
     );
   }
 
-  return parts.reduce((connectionInfo: Map, part) => {
-    const [key, value] = part.split('=');
-    connectionInfo[key] = value;
+  return parts.reduce<Record<string, string>>((connectionInfo, parameter) => {
+    const [key, value] = parameter.split('=');
+    if (value) connectionInfo[key] = value;
     return connectionInfo;
   }, {});
-}
-
-/**
- * allowed variable names: host | server | data source | datasource | addr | address
- * @param config - database config variables parsed from mysql_connection_string
- */
-export function getServerHost(config: Map): string {
-  return (
-    config.host ||
-    config.server ||
-    config['data source'] ||
-    config.datasource ||
-    config.addr ||
-    config.address ||
-    DEFAULT_HOST
-  );
-}
-
-/**
- * allowed variable names: user | user id | userid | user name | username | uid
- * @param config - database config variables parsed from mysql_connection_string
- */
-export function getUserId(config: Map): string {
-  return (
-    config.user ||
-    config['user id'] ||
-    config.userid ||
-    config['user name'] ||
-    config.username ||
-    config.uid ||
-    DEFAULT_USER
-  );
-}
-
-/**
- * Note: We are allowing no password as many FiveM servers love to not use one for an ungodly reason.
- * allowed variable names: password | pwd
- * @param config - database config variables parsed from mysql_connection_string
- */
-export function getPassword(config: Map): string | undefined {
-  const password = config.password || config.pwd;
-  if (!password) return undefined;
-  return password;
 }
