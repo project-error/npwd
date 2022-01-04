2c32bf973b327b12faaeedfbcb0c6e5d04bafa0a
Taso <taso@projecterror.dev>
2022-01-04 12:18:36 -0500

    fix(phone/utils): handle when GetParentResourceName isn't reinjected
    Due to FiveM's handling of the resource iframe, upon a reload of the resource. The custom CFX window functions are not reinjected. Although this may be fixed upstream eventually, I am not sure when.

--- phone/src/utils/misc.ts
+++ phone/src/utils/misc.ts
@@ -2,4 +2,5 @@
 export const isEnvBrowser = (): boolean =>
   process.env.NODE_ENV === 'development' && !(window as any).invokeNative;
 
-export const getResourceName = () => (window as any)?.GetParentResourceName() || 'npwd';
+export const getResourceName = () =>
+  (window as any).GetParentResourceName ? (window as any)?.GetParentResourceName() : 'npwd';
