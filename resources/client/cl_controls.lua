1038d3999d2cde9368c2df59b8a957d4a7ad8894
Dolu <47056777+dolutattoo@users.noreply.github.com>
2022-01-10 02:20:02 +0100

    Disable light melee attack (#520)

--- resources/client/cl_controls.lua
+++ resources/client/cl_controls.lua
@@ -35,6 +35,7 @@ CreateThread(function()
             DisableControlAction(0, 121, true)  -- Fly Camera (Flying)
             DisableControlAction(0, 122, true)  -- Control OVerride (Flying)
             DisableControlAction(0, 135, true)  -- Control OVerride (Sub)
+            DisableControlAction(0, 140, true)  -- Melee attack light
             DisableControlAction(0, 200, true)  -- Pause Menu
             DisableControlAction(0, 245, true)  -- Chat
         else
