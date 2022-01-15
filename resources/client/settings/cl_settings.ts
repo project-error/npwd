bc4cbce7ca858ecd54a8d874e0c59e0795f5fe87
itschip <onlygamernerd@gmail.com>
2022-01-15 19:56:45 +0100

    fix(client/settings): callVolume export'

--- resources/client/settings/cl_settings.ts
+++ resources/client/settings/cl_settings.ts
@@ -4,6 +4,6 @@ import { IPhoneSettings, SettingEvents } from '../../../typings/settings';
 // This will run once we first load NUI settings stored in localStorage, and every time
 // we update it.
 RegisterNuiCB<IPhoneSettings>(SettingEvents.NUI_SETTINGS_UPDATED, (cfg, cb) => {
-  exports['pma-voice'].setCallVolume(cfg.callVolume);
+  global.exports['pma-voice'].setCallVolume(cfg.callVolume);
   cb({});
 });
