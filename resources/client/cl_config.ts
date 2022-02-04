669133eb82e979fefb50b7f010c0ded09b8b89b0
Linden <65407488+thelindat@users.noreply.github.com>
2022-02-04 20:59:23 +1100

    feat: check convars when initialising config
    Checks for 'npwd:phoneAsItem', 'npwd:database', and 'npwd:useResourceIntegration'.

--- resources/client/cl_config.ts
+++ resources/client/cl_config.ts
@@ -1,6 +1,22 @@
 import { ResourceConfig } from '../../typings/config';
 
 // Setup and export the config for the resource
-export const config: ResourceConfig = JSON.parse(
-  LoadResourceFile(GetCurrentResourceName(), 'config.json'),
-);
+export const config = (() => {
+  let config: ResourceConfig = JSON.parse(
+    LoadResourceFile(GetCurrentResourceName(), 'config.json'),
+  );
+
+  let phoneAsItem = GetConvar('npwd:phoneAsItem', '') as string;
+  if (phoneAsItem !== '') {
+    phoneAsItem = JSON.parse(phoneAsItem) as any;
+    Object.entries(config.PhoneAsItem).forEach(([key, value]) => {
+      // @ts-ignore
+      if (phoneAsItem[key] && typeof value === typeof phoneAsItem[key]) {
+        // @ts-ignore
+        config.PhoneAsItem[key] = phoneAsItem[key];
+      }
+    });
+  }
+
+  return config;
+})();
