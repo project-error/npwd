660f77107114590ff5c7d9c50bd84462e0935d51
itschip <onlygamernerd@gmail.com>
2022-04-16 02:05:19 +0200

    fix(phone/twitter): 'explicit' null check for tweets

--- phone/src/apps/twitter/hooks/state.ts
+++ phone/src/apps/twitter/hooks/state.ts
@@ -1,4 +1,11 @@
-import { atom, useSetRecoilState, selector, useRecoilValue, useRecoilState } from 'recoil';
+import {
+  atom,
+  useSetRecoilState,
+  selector,
+  useRecoilValue,
+  useRecoilState,
+  waitForAll,
+} from 'recoil';
 import { FormattedTweet, Profile, Tweet, TwitterEvents } from '@typings/twitter';
 import fetchNui from '@utils/fetchNui';
 import { ServerPromiseResp } from '@typings/common';
@@ -47,7 +54,12 @@ export const twitterState = {
             },
           );
 
-          return resp.data?.map(processTweet);
+          if (resp.data) {
+            return resp.data?.map(processTweet);
+          }
+
+          // your mom is fat
+          return [];
         } catch (e) {
           return [];
         }
