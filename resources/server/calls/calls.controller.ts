e99051897938a1ee58c07414e5866b70eae0c7dd
Mojito-Fivem <liamdormon04@gmail.com>
2022-04-28 17:54:37 +0100

    feat(callhook): adds middleware for listening to phone calls

--- resources/server/calls/calls.controller.ts
+++ resources/server/calls/calls.controller.ts
@@ -10,8 +10,65 @@ import { getSource, onNetTyped } from '../utils/miscUtils';
 import CallService from './calls.service';
 import { callLogger } from './calls.utils';
 import { onNetPromise } from '../lib/PromiseNetEvents/onNetPromise';
+import { OnCallMap } from './middleware/onCall';
+import PlayerService from '../players/player.service';
+import MessagesService from '../messages/messages.service';
 
-onNetPromise<InitializeCallDTO, ActiveCall>(CallEvents.INITIALIZE_CALL, (reqObj, resp) => {
+onNetPromise<InitializeCallDTO, ActiveCall>(CallEvents.INITIALIZE_CALL, async (reqObj, resp) => {
+  const funcRef = OnCallMap.get(reqObj.data.receiverNumber);
+  if (funcRef) {
+    const caller = PlayerService.getPlayer(reqObj.source);
+    const incomingCaller = {
+      source: reqObj.source,
+      name: caller.getName(),
+      number: caller.getPhoneNumber(),
+    };
+    try {
+      await new Promise<void>((resolve, reject) => {
+        funcRef({
+          incomingCaller,
+          next: () => {
+            resolve();
+          },
+          exit: () => {
+            reject();
+          },
+          reply: (message) => {
+            MessagesService.handleEmitMessage({
+              senderNumber: reqObj.data.receiverNumber,
+              targetNumber: incomingCaller.number,
+              message,
+            });
+          },
+          forward: (receiverNumber) => {
+            CallService.handleInitializeCall({ ...reqObj, data: { receiverNumber } }, resp)
+              .catch((e) => {
+                resp({ status: 'error', errorMsg: 'SERVER_ERROR' });
+                callLogger.error(`Error occured handling init call: ${e.message}`);
+              })
+              .then(() => {
+                return;
+              })
+              .catch(reject);
+          },
+        });
+      });
+    } catch (e) {
+      return resp({
+        status: 'ok',
+        data: {
+          transmitter: incomingCaller.number,
+          isTransmitter: true,
+          receiver: reqObj.data.receiverNumber,
+          isUnavailable: true,
+          is_accepted: false,
+          start: Math.floor(new Date().getTime() / 1000).toString(),
+          identifier: caller.getIdentifier(),
+        },
+      });
+    }
+  }
+
   CallService.handleInitializeCall(reqObj, resp).catch((e) => {
     resp({ status: 'error', errorMsg: 'SERVER_ERROR' });
     callLogger.error(`Error occured handling init call: ${e.message}`);
