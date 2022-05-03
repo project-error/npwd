caf89d8bb758dc54d9564d2c7f2438d6699a9662
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2022-05-03 06:00:12 +0000

    chore(bump-manifest): Bump manifest version to 1.2.98

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 game "gta5"
 description 'js runtime monkaW'
 authors { "itschip",  "erik-sn", "TasoOneAsia", "kidz", "RockySouthpaw"}
-version '1.2.97'
+version '1.2.98'
 
 client_scripts {
     'resources/dist/client/client.js',
