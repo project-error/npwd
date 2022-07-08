73cc54fd5f2aae66ac87adc4d7939f4ef6183cc0
Kyle McShea <46171436+kylemcshea@users.noreply.github.com>
2022-07-08 00:12:11 -0400

    chore | get rid of unused code.

--- phone/src/apps/taskrabbit/components/TaskRabbitApp.tsx
+++ phone/src/apps/taskrabbit/components/TaskRabbitApp.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { List } from '@ui/components/List';
-// import { useApp } from '@os/apps/hooks/useApps';
 import { useTaskRabbitListings } from '../hooks/state';
 import { TaskRabbitListing } from './TaskRabbitListing';
 
 export const TaskRabbitApp: React.FC = () => {
-  // const taskRabbit = useApp('TASKRABBIT');
-
   const availableJobs = useTaskRabbitListings();
 
   return (
