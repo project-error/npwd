56c0579b688dde1be033f652d0f10553563d2e76
Boris <boris@borisnl.nl>
2022-07-28 12:00:36 +0200

    fix: change image filtering

--- resources/server/utils/imageFiltering.ts
+++ resources/server/utils/imageFiltering.ts
@@ -1,7 +1,5 @@
 import { config } from '../config';
 
-const imageRegex = new RegExp(config.imageSafety.safeImageUrls.join('|'));
-
 export const checkAndFilterImage = (imageUrl: string): string | null => {
   const image = imageUrl.trim();
   if (image == '') {
@@ -11,8 +9,9 @@ export const checkAndFilterImage = (imageUrl: string): string | null => {
   // If we don't care about filtering images just return the image
   if (!config.imageSafety.filterUnsafeImageUrls) return image;
 
+  const hostname = new URL(imageUrl).hostname;
   // We have an allowed image url!
-  if (imageRegex.test(image)) return image;
+  if (config.imageSafety.safeImageUrls.includes(hostname)) return image;
 
   // Embed unsafe urls so we don't accidentally expose our users IP address
   if (config.imageSafety.embedUnsafeImages) return `${config.imageSafety.embedUrl}?url=${imageUrl}`;
