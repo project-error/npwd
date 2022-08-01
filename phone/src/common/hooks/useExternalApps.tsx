16e16e3b1f11976050c0c7f3ebed7f4b98c6210b
Anton Stjernquist <antonstjernquist@gmail.com>
2022-08-01 17:50:26 +0200

    fix: invalid app icon & types

--- phone/src/common/hooks/useExternalApps.tsx
+++ phone/src/common/hooks/useExternalApps.tsx
@@ -3,31 +3,59 @@ import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import { useSettings } from '@apps/settings/hooks/useSettings';
 import { useCustomEvent } from '@os/events/useCustomEvents';
+import { QuestionMark } from '@mui/icons-material';
 
-const externalApps = require('../../../../config.apps');
+import externalApps from '../../../../config.apps';
+
+const InvalidAppConfig = (id: string): IApp => ({
+  id,
+  nameLocale: `Invalid external app with id "${id}"`,
+  icon: <QuestionMark />,
+  Route: () => null,
+  backgroundColor: '#222',
+  color: '#fff',
+  isDisabled: false,
+  notification: {
+    badge: 0,
+    icon: <QuestionMark />,
+    key: '',
+  },
+  NotificationIcon: null,
+  notificationIcon: null,
+  path: '',
+  disable: false,
+});
 
 const useExternalAppsAction = () => {
-  const generateAppConfig = async (importStatement): Promise<IApp> => {
-    const rawConfig = (await importStatement()).default;
-    const config = typeof rawConfig === 'function' ? rawConfig({ language: 'sv' }) : rawConfig;
+  const generateAppConfig = async (
+    importStatement: CallableFunction,
+    id: string,
+  ): Promise<IApp> => {
+    try {
+      const rawConfig = (await importStatement()).default;
+      const config = typeof rawConfig === 'function' ? rawConfig({ language: 'sv' }) : rawConfig;
 
-    config.Component = (props: object) => React.createElement(config.app, props);
+      config.Component = (props: object) => React.createElement(config.app, props);
 
-    config.icon = React.createElement(config.icon);
+      config.icon = React.createElement(config.icon);
 
-    config.Route = (props: object) => (
-      <Route path={config.path}>
-        <config.Component {...props} />
-      </Route>
-    );
+      config.Route = (props: object) => (
+        <Route path={config.path}>
+          <config.Component {...props} />
+        </Route>
+      );
 
-    return config;
+      return config;
+    } catch (error: unknown) {
+      console.error('Failed to load external app.', error);
+      return InvalidAppConfig(id);
+    }
   };
 
-  const getConfigs = async (communityApps) => {
+  const getConfigs = async (externalApps: Record<string, CallableFunction>) => {
     const configs = await Promise.all(
-      Object.entries(communityApps).map(async ([key, value]) => {
-        const app = await generateAppConfig(value);
+      Object.entries(externalApps).map(async ([key, value]) => {
+        const app = await generateAppConfig(value, key);
         return app;
       }),
     );
