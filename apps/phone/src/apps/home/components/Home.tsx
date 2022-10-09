321609be9f58001c9b431e20aec0c8283973326f
MM <mm.jogos.game@gmail.com>
2022-10-10 00:26:26 +0100

    fix(phone/home): only use cached external apps instead of loading them

--- apps/phone/src/apps/home/components/Home.tsx
+++ apps/phone/src/apps/home/components/Home.tsx
@@ -3,11 +3,12 @@ import { AppWrapper } from '@ui/components';
 import { Box } from '@mui/material';
 import { GridMenu } from '@ui/components/GridMenu';
 import { useApps } from '@os/apps/hooks/useApps';
-import { useExternalApps } from '@common/hooks/useExternalApps';
+import { useRecoilValue } from 'recoil';
+import { phoneState } from '@os/phone/hooks/state';
 
 export const HomeApp: React.FC = () => {
   const { apps } = useApps();
-  const externalApps = useExternalApps();
+  const externalApps = useRecoilValue(phoneState.extApps).filter(Boolean);
   return (
     <AppWrapper>
       <Box component="div" mt={6} px={1}>
