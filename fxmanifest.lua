4540461ff798bdf8c8bf01059a6358162da2ad32
itschip <chip@projecterror.dev>
2022-10-22 11:50:44 +0200

    fix(fxmanifest): correct version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,29 +1,29 @@
-fx_version "cerulean"
-game "gta5"
-description 'js runtime monkaW'
-authors { "itschip",  "erik-sn", "TasoOneAsia", "kidz", "RockySouthpaw"}
-version '1.0.8'
-client_scripts {
+fx_version("cerulean")
+game("gta5")
+description("js runtime monkaW")
+authors({ "itschip", "erik-sn", "TasoOneAsia", "kidz", "RockySouthpaw" })
+version("1.7.0")
+client_scripts({
 	"resources/dist/client/client.js",
 	"resources/client/*.lua",
-}
+})
 
-server_script {
+server_script({
 	-- This is a file that lives purely in source code and isn't compiled alongside
 	-- rest of the release. It's used to detect whether a user can read or not.
 	"build-detector.js",
 	"resources/dist/server/server.js",
-}
+})
 
-ui_page "resources/html/index.html"
+ui_page("resources/html/index.html")
 
-files {
+files({
 	"config.json",
 	"resources/html/index.html",
 	"resources/html/**/*",
-}
+})
 
-dependency {
+dependency({
 	"screenshot-basic",
 	"pma-voice",
-}
+})
