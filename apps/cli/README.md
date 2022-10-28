6b12103b930e74a13fa437709f019651ecbaae04
itschip <chip@projecterror.dev>
2022-10-28 16:27:51 +0200

    feat(server/messages): trigger onMessage func ref on emitMessage export

---
+++ apps/cli/README.md
@@ -0,0 +1,3 @@
+# NPWD CLI
+
+This is the CLI for NPWD that can be use to scaffold new third party apps
--- apps/game/server/messages/middleware/emitMessage.ts
+++ apps/game/server/messages/middleware/emitMessage.ts
@@ -1,5 +1,8 @@
-import { EmitMessageExportCtx } from '../../../../typings/messages';
+import { EmitMessageExportCtx } from '@typings/messages';
+import PlayerService from '../../players/player.service';
 import MessagesService from '../messages.service';
+import { messagesLogger } from '../messages.utils';
+import { OnMessageExportMap } from './onMessage';
 
 const exp = global.exports;
 
@@ -10,5 +13,23 @@ exp('emitMessage', async ({ senderNumber, targetNumber, message, embed }: EmitMe
     targetNumber,
     message,
     embed: embed && JSON.stringify(embed),
+  }).then(async () => {
+    const funcRef = OnMessageExportMap.get(targetNumber);
+
+    const senderIdentifier = await PlayerService.getIdentifierByPhoneNumber(senderNumber, false);
+    const senderPlayer = PlayerService.getPlayerFromIdentifier(senderIdentifier);
+
+    if (funcRef) {
+      try {
+        await funcRef({
+          data: { embed, message, sourcePhoneNumber: senderNumber, tgtPhoneNumber: targetNumber },
+          source: senderPlayer.source,
+        });
+      } catch (e) {
+        messagesLogger.error(
+          `Failed to find a callback reference for onMessage. Probably because the resource(s) using the export was stopped or restarted. Please restart said resource(s). Error: ${e.message}`,
+        );
+      }
+    }
   });
 });
