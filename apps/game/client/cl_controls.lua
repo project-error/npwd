1d4234211fe20334ab7c8024b76dcc8b64393ed5
mikigoalie <mikigoalie@seznam.cz>
2023-04-25 03:27:30 +0200

    update(client/cl_controls.lua): optimize thread

--- apps/game/client/cl_controls.lua
+++ apps/game/client/cl_controls.lua
@@ -1,64 +1,46 @@
+local controlsDisabled = false
 local disableKeys
-
 local DisableControlAction = DisableControlAction
+local EnableControlAction = EnableControlAction
 local Wait = Wait
 local CreateThread = CreateThread
-
-CreateThread(function()
+local _npwd = exports.npwd
+local function pausemenuThread()
+    local sleep = 1000
     while true do
-        if disableKeys then
-            Wait(0)
-            DisableControlAction(0, 0, true)    -- Next Camera
-            DisableControlAction(0, 1, true)    -- Look Left/Right
-            DisableControlAction(0, 2, true)    -- Look up/Down
-            DisableControlAction(0, 16, true)   -- Next Weapon
-            DisableControlAction(0, 17, true)   -- Select Previous Weapon
-            DisableControlAction(0, 22, true)   -- Jump
-            DisableControlAction(0, 24, true)   -- Attack
-            DisableControlAction(0, 25, true)   -- Aim
-            DisableControlAction(0, 26, true)   -- Look Behind
-            DisableControlAction(0, 36, true)   -- Input Duck/Sneak
-            DisableControlAction(0, 37, true)   -- Weapon Wheel
-            DisableControlAction(0, 44, true)   -- Cover
-            DisableControlAction(0, 47, true)   -- Detonate
-            DisableControlAction(0, 55, true)   -- Dive
-            DisableControlAction(0, 75, true)   -- Exit Vehicle
-            DisableControlAction(0, 76, true)   -- Vehicle Handbrake
-            DisableControlAction(0, 81, true)   -- Next Radio (Vehicle)
-            DisableControlAction(0, 82, true)   -- Previous Radio (Vehicle)
-            DisableControlAction(0, 91, true)   -- Passenger Aim (Vehicle)
-            DisableControlAction(0, 92, true)   -- Passenger Attack (Vehicle)
-            DisableControlAction(0, 99, true)   -- Select Next Weapon (Vehicle)
-            DisableControlAction(0, 106, true)  -- Control Override (Vehicle)
-            DisableControlAction(0, 114, true)  -- Fly Attack (Flying)
-            DisableControlAction(0, 115, true)  -- Next Weapon (Flying)
-            DisableControlAction(0, 121, true)  -- Fly Camera (Flying)
-            DisableControlAction(0, 122, true)  -- Control OVerride (Flying)
-            DisableControlAction(0, 135, true)  -- Control OVerride (Sub)
-            DisableControlAction(0, 140, true)  -- Melee attack light
-            DisableControlAction(0, 200, true)  -- Pause Menu
-            DisableControlAction(0, 245, true)  -- Chat
-        else
-            Wait(100)
+        if not _npwd.isPhoneVisible() then
+            sleep = 1000
+            goto continue
         end
-    end
-end)
 
--- Handles pause menu state
-CreateThread(function()
-    while true do
-        Wait(500)
+        sleep = 500
 
-        local isPauseOpen = IsPauseMenuActive() ~= false
-        local isPhoneVisible = exports.npwd:isPhoneVisible()
-        -- Handle if the phone is already visible and escape menu is opened
-        if isPauseOpen and isPhoneVisible then
-            exports.npwd:setPhoneVisible(false)
+        if IsPauseMenuActive() == false then
+            goto continue
         end
-    end
-end)
 
+        _npwd:setPhoneVisible(false)
+        :: continue ::
+        Wait(sleep)
+    end
+end CreateThread(pausemenuThread)
+
+
+local function blockActions()
+    while controlsDisabled do
+		DisableAllControlActions(1)		  
+		DisableAllControlActions(2)
+        EnableControlAction(0, 22, true)
+        EnableControlAction(0, 30, true)
+        EnableControlAction(0, 31, true)
+        EnableControlAction(0, 32, true)
+        EnableControlAction(0, 33, true)
+        Wait(0)
+    end
+    collectgarbage("collect")
+end
 
 AddEventHandler('npwd:disableControlActions', function(bool)
-  disableKeys = bool
+    controlsDisabled = bool
+    CreateThread(blockActions)
 end)
