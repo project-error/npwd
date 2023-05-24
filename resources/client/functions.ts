52ee33ffbe9b484b3899137fe2abfd4b69ef94cb
fkk-cz <certified.gamiie@gmail.com>
2023-05-23 23:28:41 -0400

    tweak(client): use local object creation for phone prop

--- resources/client/functions.ts
+++ resources/client/functions.ts
@@ -5,6 +5,8 @@ import { Delay } from '../utils/fivem';
 let prop = 0;
 let propCreated = false;
 
+const plyEntMap: Map<number, number> = new Map();
+
 /* * * * * * * * * * * * *
  *
  *  Prop Deletion/Creation handling
@@ -13,31 +15,60 @@ let propCreated = false;
 
 // TODO: add a option to make server side for people who use entity lockdown.
 
-export const newPhoneProp = async () => {
-  const hasNPWDProps = GetConvarInt('NPWD_PROPS', 0);
-  let phoneModel;
-  if (hasNPWDProps) {
-    phoneModel = KvpService.getKvpString(KvpItems.NPWD_FRAME) || 'prop_npwd_default';
-  } else {
-    phoneModel = 'prop_amb_phone';
-  }
-  removePhoneProp(); //deletes the already existing prop before creating another.
-  if (!propCreated) {
-    RequestModel(phoneModel);
+AddStateBagChangeHandler(
+  'phonePropData',
+  null as any,
+  async (
+    bagName: string,
+    key: string,
+    value: any | null,
+    _reserved: number,
+    replicated: boolean,
+  ) => {
+    const player = GetPlayerFromStateBagName(bagName);
+    if (player === 0) {
+      return;
+    }
 
+    // Only execute handler once for local client
+    if (replicated) return;
+
+    const serverId = GetPlayerServerId(player);
+    // Scope here so we don't get an error trying to use ent
+    {
+      // Check if we already have an entity for this person, if we do then delete it.
+      const ent = plyEntMap.get(serverId);
+      if (ent) {
+        DeleteEntity(ent);
+        plyEntMap.delete(serverId);
+      }
+    }
+
+    // We don't have any PropData so we don't want to do anything else
+    if (!value) return;
+
+    const hasNPWDProps = GetConvarInt('NPWD_PROPS', 0);
+    let phoneModel;
+    if (hasNPWDProps) {
+      phoneModel = KvpService.getKvpString(KvpItems.NPWD_FRAME) || 'prop_npwd_default';
+    } else {
+      phoneModel = 'prop_amb_phone';
+    }
+
+    RequestModel(phoneModel);
     while (!HasModelLoaded(phoneModel)) {
       await Delay(1);
     }
 
-    const playerPed = PlayerPedId();
+    const playerPed = GetPlayerPed(player);
     const [x, y, z] = GetEntityCoords(playerPed, true);
-    prop = CreateObject(GetHashKey(phoneModel), x, y, z + 0.2, true, true, true);
-    //prop = CreateObject(GetHashKey(phoneModel), 1.0, 1.0, 1.0, 1, 1, 0)
-    const boneIndex = GetPedBoneIndex(playerPed, 28422);
+
+    const prop = CreateObject(GetHashKey(phoneModel), x, y, z, false, false, false);
+    plyEntMap.set(serverId, prop);
     AttachEntityToEntity(
       prop,
       playerPed,
-      boneIndex,
+      GetPedBoneIndex(playerPed, 28422),
       0.0,
       0.0,
       0.0,
@@ -50,18 +81,21 @@ export const newPhoneProp = async () => {
       true,
       1.0,
       true,
-    ); //-- Attaches the phone to the player.
-    propCreated = true;
-  } else if (propCreated) {
-    console.log('prop already created');
+    );
+  },
+);
+
+onNet('onPlayerDropped', async (serverId: number) => {
+  if (plyEntMap.has(serverId)) {
+    DeleteEntity(plyEntMap.get(serverId));
+    plyEntMap.delete(serverId);
   }
+});
+
+export const newPhoneProp = async () => {
+  LocalPlayer.state.set('phonePropData', true, true);
 };
 
 export function removePhoneProp() {
-  //-- Triggered in newphoneProp function. Only way to destory the prop correctly.
-  if (prop != 0) {
-    DeleteEntity(prop);
-    prop = 0;
-    propCreated = false;
-  }
+  LocalPlayer.state.set('phonePropData', null, true);
 }
