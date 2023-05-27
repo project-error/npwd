56fca896c3b781ca8855c89c76a2caaf52652ad0
Melissa G <10230512-GlitchX_x@users.noreply.gitlab.com>
2023-05-27 14:09:52 -0400

    clean: refactored some clutter in twitter message checks

--- apps/phone/src/apps/twitter/components/AddTweetModal.tsx
+++ apps/phone/src/apps/twitter/components/AddTweetModal.tsx
@@ -80,13 +80,7 @@ const AddTweetModal = () => {
   const submitTweet = async () => {
     await promiseTimeout(200);
     const cleanedMessage = clean(message.trim());
-    if (cleanedMessage.length === 0) {
-      if (!allowNoMessage || !images || images.length === 0)
-      {
-        return
-      }
-    }
-
+    if ((!cleanedMessage && !allowNoMessage) || !images || images.length === 0) return;
     if (cleanedMessage.length > 0 && !isValidMessage(cleanedMessage)) return;
 
     const data: NewTweet = {
