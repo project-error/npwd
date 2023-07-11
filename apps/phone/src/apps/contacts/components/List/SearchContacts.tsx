3fb9ba05d5aa4bd05f59bd440e3dfb430e7d4493
BackSH00TER <18689469+BackSH00TER@users.noreply.github.com>
2023-07-10 21:08:39 -0700

    fix search box on contactsapp

--- apps/phone/src/apps/contacts/components/List/SearchContacts.tsx
+++ apps/phone/src/apps/contacts/components/List/SearchContacts.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
+import { Box } from '@mui/material';
+import { SearchField } from '@ui/components/SearchField';
 import { useDebounce } from '@os/phone/hooks/useDebounce';
 import { useSetContactFilterInput } from '../../hooks/state';
-import { Search } from 'lucide-react';
 
 export const SearchContacts: React.FC = () => {
   const [t] = useTranslation();
@@ -18,16 +19,12 @@ export const SearchContacts: React.FC = () => {
   }, [debouncedVal, setFilterVal]);
 
   return (
-    <div className="w-full py-2 px-4">
-      <div className="flex items-center justify-start bg-neutral-200 dark:bg-neutral-800 rounded-md px-2 space-x-2">
-        <Search className="h-5 w-5 dark:text-neutral-400" />
-        <input
-          className="w-full text-base dark:text-neutral-100 py-2 bg-transparent outline-none "
-          onChange={(e) => setInputVal(e.target.value)}
-          placeholder={t('CONTACTS.PLACEHOLDER_SEARCH_CONTACTS')}
-          value={inputVal}
-        />
-      </div>
-    </div>
+    <Box>
+      <SearchField
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
+        placeholder={t('CONTACTS.PLACEHOLDER_SEARCH_CONTACTS')}
+      />
+    </Box>
   );
 };
