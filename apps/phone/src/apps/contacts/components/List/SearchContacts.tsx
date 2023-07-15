16003c72cb76d6d074c483e0e4e4822d9e9a9a68
itschip <onlygamernerd@gmail.com>
2023-07-15 19:48:05 +0200

    feat(apps/phone): add tailwind input with toggle keys for contact search

--- apps/phone/src/apps/contacts/components/List/SearchContacts.tsx
+++ apps/phone/src/apps/contacts/components/List/SearchContacts.tsx
@@ -2,29 +2,28 @@ import React, { useEffect, useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { Box } from '@mui/material';
-import { SearchField } from '@ui/components/SearchField';
 import { useDebounce } from '@os/phone/hooks/useDebounce';
 import { useSetContactFilterInput } from '../../hooks/state';
+import { NPWDSearchInput } from "@ui/components";
 
 export const SearchContacts: React.FC = () => {
-  const [t] = useTranslation();
-  const setFilterVal = useSetContactFilterInput();
-  const [inputVal, setInputVal] = useState('');
-
-  const debouncedVal = useDebounce<string>(inputVal, 500);
-
-  useEffect(() => {
-    setFilterVal(debouncedVal);
-  }, [debouncedVal, setFilterVal]);
-
-  return (
-    <Box>
-      <SearchField
-        value={inputVal}
-        onChange={(e) => setInputVal(e.target.value)}
-        placeholder={t('CONTACTS.PLACEHOLDER_SEARCH_CONTACTS')}
-      />
-    </Box>
-  );
+	const [t] = useTranslation();
+	const setFilterVal = useSetContactFilterInput();
+	const [inputVal, setInputVal] = useState('');
+	
+	const debouncedVal = useDebounce<string>(inputVal, 500);
+	
+	useEffect(() => {
+		setFilterVal(debouncedVal);
+	}, [debouncedVal, setFilterVal]);
+	
+	return (
+		<div className="w-full py-2 px-4">
+			<NPWDSearchInput
+				onChange={(e) => setInputVal(e.target.value)}
+				placeholder={t('CONTACTS.PLACEHOLDER_SEARCH_CONTACTS')}
+				value={inputVal}
+			/>
+		</div>
+	);
 };
--- apps/phone/src/ui/components/Input.tsx
+++ apps/phone/src/ui/components/Input.tsx
@@ -5,6 +5,7 @@ import { PhoneEvents } from '@typings/phone';
 import fetchNui from '@utils/fetchNui';
 import { cva, VariantProps } from 'class-variance-authority';
 import { twMerge } from 'tailwind-merge';
+import { Search } from "lucide-react";
 
 export const toggleKeys = (keepGameFocus: boolean) =>
   fetchNui(
@@ -72,5 +73,44 @@ export const classes = cva('rounded-md outline-none w-full', {
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & VariantProps<typeof classes>;
 
 export const NPWDInput: React.FC<InputProps> = ({ size, variant, className, ...props }) => {
-  return <input {...props} className={twMerge(classes({ size, variant, className }))} />;
+  return <input
+	  {...props}
+	  className={twMerge(classes({ size, variant, className }))}
+	  onMouseUp={(e) => {
+		  toggleKeys(false);
+		  if (props.onMouseUp) {
+			  props.onMouseUp(e);
+		  }
+	  }}
+	  onBlur={(e) => {
+		  toggleKeys(true);
+		  if (props.onBlur) {
+			  props.onBlur(e);
+		  }
+	  }}
+  />;
 };
+
+export const NPWDSearchInput: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
+	return (
+		<div className="flex items-center justify-start bg-neutral-200 dark:bg-neutral-800 rounded-md px-2 space-x-2 border dark:border-neutral-700 border-neutral-300">
+			<Search className="h-5 w-5 dark:text-neutral-400"/>
+			<input
+				{...props}
+				className="w-full text-base dark:text-neutral-100 py-2 bg-transparent outline-none"
+				onMouseUp={(e) => {
+					toggleKeys(false);
+					if (props.onMouseUp) {
+						props.onMouseUp(e);
+					}
+				}}
+				onBlur={(e) => {
+					toggleKeys(true);
+					if (props.onBlur) {
+						props.onBlur(e);
+					}
+				}}
+			/>
+		</div>
+	)
+}
