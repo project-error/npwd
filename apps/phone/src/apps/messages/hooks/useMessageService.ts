a0cfdf280ca21b79b4a8e1d062ea4777d9836cc3
BackSH00TER <18689469+BackSH00TER@users.noreply.github.com>
2023-07-19 22:28:32 -0700

    fix notif not appearing

--- apps/phone/src/apps/messages/hooks/useMessageService.ts
+++ apps/phone/src/apps/messages/hooks/useMessageService.ts
@@ -3,12 +3,11 @@ import { Message, MessageConversation, MessageEvents } from '@typings/messages';
 import { useMessageActions } from './useMessageActions';
 import { useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
-import { messageState, useActiveMessageConversation } from './state';
+import { useActiveMessageConversation } from './state';
 import { usePhoneVisibility } from '@os/phone/hooks';
 import { useNotification } from '@os/new-notifications/useNotification';
 import { useContactActions } from '@apps/contacts/hooks/useContactActions';
 import useMessages from './useMessages';
-import { useRecoilValue } from 'recoil';
 
 export const useMessagesService = () => {
   const { updateLocalMessages, updateLocalConversations, setMessageReadState } =
@@ -19,19 +18,18 @@ export const useMessagesService = () => {
   const { enqueueNotification } = useNotification();
   const { getDisplayByNumber } = useContactActions();
   const { getMessageConversationById, goToConversation } = useMessages();
-  const activeMessageConversation = useRecoilValue(messageState.activeMessageConversation);
 
   const addNotification = useCallback(
-    (message: string, convoName: string, convoId: number) => {
-      const group = getMessageConversationById(convoId);
+    (message: string, convName: string, convId: number) => {
+      const group = getMessageConversationById(convId);
 
       enqueueNotification({
         appId: 'MESSAGES',
         notisId: 'npwd:messages:messageBroadcast',
         content: message,
         onClick: () => goToConversation(group),
-        secondaryTitle: getDisplayByNumber(convoName) ?? convoName,
-        path: `/messages/conversations/${activeMessageConversation.id}`,
+        secondaryTitle: getDisplayByNumber(convName) ?? convName,
+        path: `/messages/conversations/${convId}`,
       });
     },
     [enqueueNotification],
@@ -44,14 +42,13 @@ export const useMessagesService = () => {
     
     setMessageReadState(conversation_id, 1);
     // Set the current unread count to 1, when they click it will be removed
-    //
     addNotification(message, conversationName, conversation_id);
   };
 
   // This is only called for the receiver of the message. We'll be using the standardized pattern for the transmitter.
   const handleUpdateMessages = useCallback(
     (messageDto: Message) => {
-      if (messageDto.conversation_id !== activeConversation.id) return;
+      if (messageDto.conversation_id !== activeConversation?.id) return;
 
       updateLocalMessages(messageDto);
     },
