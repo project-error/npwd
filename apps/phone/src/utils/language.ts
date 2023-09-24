cc8c9668e7dd83ee0d86babb950ee4490d394eba
itschip <chip@projecterror.dev>
2023-09-24 15:40:09 +0200

    fix(phone/language): reutrn default if browser env

--- apps/phone/src/utils/language.ts
+++ apps/phone/src/utils/language.ts
@@ -1,11 +1,14 @@
 import phoneConfig from '../config/default.json';
 import { fetchConfig } from './config';
+import { isEnvBrowser } from './misc';
 
 export const getDefaultLanguage = async () => {
+  if (isEnvBrowser()) return phoneConfig.defaultSettings.language;
+
   const mainConfig = await fetchConfig();
   const defaultLanguage = phoneConfig?.languages?.find(
     (language) => language.value === mainConfig?.general.defaultLanguage,
   );
 
-  return defaultLanguage ?? phoneConfig.defaultSettings.language;
+  return defaultLanguage;
 };
