7dd5cfafa670c99d5606132ca3bb65857609a94e
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 18:07:27 +0000

    chore: bump manifest version to nightly 2.0.0-beta.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version("cerulean")
 game("gta5")
 description("js runtime monkaW")
 authors({ "itschip", "erik-sn", "TasoOneAsia", "kidz", "RockySouthpaw", "SamShanks", "c-wide", "mojito" })
-version("1.8.7")
+version("2.0.0-beta.0")
 client_scripts({
 	"dist/game/client/client.js",
 	"dist/game/client/*.lua",
