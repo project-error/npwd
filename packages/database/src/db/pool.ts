92651152295fa2d9ac556c68e2a7e20705d51c72
itschip <chip@projecterror.dev>
2024-01-19 21:03:22 +0100

    chore(packages/database): remove debug logs

--- packages/database/src/db/pool.ts
+++ packages/database/src/db/pool.ts
@@ -1,16 +1,16 @@
 import mysql from 'mysql2/promise';
-import { CONNECTION_STRING } from './db_utils';
-import { mainLogger } from '@npwd/logger/server';
-import { parseUri2 } from './parseUri';
+import {CONNECTION_STRING} from './db_utils';
+import {mainLogger} from '@npwd/logger/server';
+import {parseUri2} from './parseUri';
 
 // we require set mysql_connection_string  to be set in the config
 const mysqlConnectionString = GetConvar(CONNECTION_STRING, 'none');
 if (mysqlConnectionString === 'none') {
-  const error = new Error(
-    `No connection string provided. make sure "${CONNECTION_STRING}" is set in your config.`,
-  );
-  mainLogger.error(error.message);
-  throw error;
+    const error = new Error(
+        `No connection string provided. make sure "${CONNECTION_STRING}" is set in your config.`,
+    );
+    mainLogger.error(error.message);
+    throw error;
 }
 
 /**
@@ -22,74 +22,71 @@ if (mysqlConnectionString === 'none') {
  * https://brouznouf.github.io/fivem-mysql-async/config/ and we need to handle both of them.
  */
 export function generateConnectionPool() {
-  try {
-    console.log('mysqlConnectionString', mysqlConnectionString)
+    try {
+        const config = mysqlConnectionString.includes('mysql://')
+            ? parseUri2(mysqlConnectionString)
+            : mysqlConnectionString
+                .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
+                .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
+                .replace(/(?:pwd|pass)=/gi, 'password=')
+                .replace(/(?:db)=/gi, 'database=')
+                .split(';')
+                .reduce<Record<string, string>>((connectionInfo, parameter) => {
+                    const [key, value] = parameter.split('=');
+                    if (key) connectionInfo[key] = value;
+                    return connectionInfo;
+                }, {});
 
-    const config = mysqlConnectionString.includes('mysql://')
-      ? parseUri2(mysqlConnectionString)
-      : mysqlConnectionString
-          .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
-          .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
-          .replace(/(?:pwd|pass)=/gi, 'password=')
-          .replace(/(?:db)=/gi, 'database=')
-          .split(';')
-          .reduce<Record<string, string>>((connectionInfo, parameter) => {
-            const [key, value] = parameter.split('=');
-            if (key) connectionInfo[key] = value;
-            return connectionInfo;
-          }, {});
 
-    console.log("db config", config);
-
-    return mysql.createPool({
-      ...config,
-      connectTimeout: 60000,
-      host: config.host,
-      database: config.database,
-      port: parseInt(config.port as string, 10),
-      user: config.user,
-      password: config.password,
-    });
-  } catch (e) {
-    mainLogger.error(`SQL Connection Pool Error: ${e.message}`, {
-      connection: mysqlConnectionString,
-    });
-  }
+        return mysql.createPool({
+            ...config,
+            connectTimeout: 60000,
+            host: config.host,
+            database: config.database,
+            port: parseInt(config.port as string, 10),
+            user: config.user,
+            password: config.password,
+        });
+    } catch (e) {
+        mainLogger.error(`SQL Connection Pool Error: ${e.message}`, {
+            connection: mysqlConnectionString,
+        });
+    }
 }
 
 
 export const getDbConfig = () => {
-  const config = mysqlConnectionString.includes('mysql://')
-      ? parseUri2(mysqlConnectionString)
-      : mysqlConnectionString
-          .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
-          .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
-          .replace(/(?:pwd|pass)=/gi, 'password=')
-          .replace(/(?:db)=/gi, 'database=')
-          .split(';')
-          .reduce<Record<string, string>>((connectionInfo, parameter) => {
-            const [key, value] = parameter.split('=');
-            if (key) connectionInfo[key] = value;
-            return connectionInfo;
-          }, {});
+    const config = mysqlConnectionString.includes('mysql://')
+        ? parseUri2(mysqlConnectionString)
+        : mysqlConnectionString
+            .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
+            .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
+            .replace(/(?:pwd|pass)=/gi, 'password=')
+            .replace(/(?:db)=/gi, 'database=')
+            .split(';')
+            .reduce<Record<string, string>>((connectionInfo, parameter) => {
+                const [key, value] = parameter.split('=');
+                if (key) connectionInfo[key] = value;
+                return connectionInfo;
+            }, {});
 
-  return config;
+    return config;
 }
 export const pool = generateConnectionPool();
 
 export async function withTransaction(queries: Promise<any>[]): Promise<any[]> {
-  const connection = await pool.getConnection();
-  connection.beginTransaction();
+    const connection = await pool.getConnection();
+    connection.beginTransaction();
 
-  try {
-    const results = await Promise.all(queries);
-    await connection.commit();
-    await connection.release();
-    return results;
-  } catch (err) {
-    mainLogger.warn(`Error when submitting queries in transaction, ${err.message}`);
-    await connection.rollback();
-    await connection.release();
-    return Promise.reject(err);
-  }
+    try {
+        const results = await Promise.all(queries);
+        await connection.commit();
+        await connection.release();
+        return results;
+    } catch (err) {
+        mainLogger.warn(`Error when submitting queries in transaction, ${err.message}`);
+        await connection.rollback();
+        await connection.release();
+        return Promise.reject(err);
+    }
 }
