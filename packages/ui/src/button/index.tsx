6749d012326e40f28331d08a318ef096a526a786
itschip <chip@projecterror.dev>
2024-01-27 01:03:11 +0100

    fix(ui/button): remove unsued import

--- packages/ui/src/button/index.tsx
+++ packages/ui/src/button/index.tsx
@@ -1,26 +1,28 @@
 import { cva, VariantProps } from 'class-variance-authority';
 import React from 'react';
-import { twMerge } from 'tailwind-merge';
 import { cn } from '../utils';
 
-const classes = cva('rounded text-white dark:text-white inline-flex items-center justify-center whitespace-nowrap font-medium transition', {
-  variants: {
-    variant: {
-      primary: ['bg-blue-500'],
-      ghost: ['bg-transparent hover:bg-neutral-200 hover:dark:bg-neutral-800'],
+const classes = cva(
+  'rounded text-white dark:text-white inline-flex items-center justify-center whitespace-nowrap font-medium transition',
+  {
+    variants: {
+      variant: {
+        primary: ['bg-blue-500'],
+        ghost: ['bg-transparent hover:bg-neutral-200 hover:dark:bg-neutral-800'],
+      },
+      size: {
+        sm: 'px-2 py-1.5 text-sm',
+        md: 'h-10 px-4 py-2 text-base',
+        lg: 'px-4 py-2',
+        icon: 'h-10 w-10',
+      },
     },
-    size: {
-      sm: 'px-2 py-1.5 text-sm',
-      md: 'h-10 px-4 py-2 text-base',
-      lg: 'px-4 py-2',
-      icon: 'h-10 w-10'
+    defaultVariants: {
+      size: 'md',
+      variant: 'primary',
     },
   },
-  defaultVariants: {
-    size: 'md',
-    variant: 'primary',
-  },
-});
+);
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof classes>;
